fc7f730a4d0088e445defe0c8ee5a9a6cf53fd06
Dr. Greg Wettstein <greg@hooter.(none)>
2010-02-21 11:47:13 -0600

    Begin implementation of query client and root referral server.
    The root referral server is the most fundamental element of the
    identity brokerage infra-structure.  It is responsible for fielding
    the identity query, authenticating the user and device and forwarding
    the target identitie(s) to the brokerage servers which resolve
    the holders of the identities.
    
    The query client implements the formation of the identity query request
    by encrypting the target identities in keys which can only be decrypted
    by the user and device identity brokerage servers.

---
+++ query-client.c
@@ -0,0 +1,207 @@
+/** \file
+ * This file contains an implementation of the identity query client.
+ * This application is responsible for initating an identity query
+ * onto the identity referral network.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2010, Enjellic Systems Development. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+
+/* Include files. */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <Config.h>
+#include <Buffer.h>
+
+#include "NAAAIM.h"
+#include "Duct.h"
+#include "IDtoken.h"
+#include "Authenticator.h"
+
+
+/**
+ * Private function.
+ *
+ * This function parses an identity from the specified file.
+ *
+ * \param token		A token parsing object into which the identity
+ *			is to be loaded.
+ *
+ * \param idfile	A pointer to a null terminated buffer containing
+ *			the name of the identity token file to be
+ *			parsed.
+ *
+ * \return		A boolean value is used to return the success or
+ *			failure of the token loading.  A true value is
+ *			used to indicate success.
+ */
+
+static _Bool load_identity(const IDtoken token, const char * const idfile)
+
+{
+	auto _Bool retn = false;
+
+	auto FILE *infile = NULL;
+
+
+	if ( (infile = fopen(idfile, "r")) == NULL )
+		goto done;
+
+	if ( !token->parse(token, infile) )
+		goto done;
+
+	retn = true;
+
+
+ done:
+	if ( infile != NULL )
+		fclose(infile);
+	return retn;
+}
+
+
+/*
+ * Program entry point begins here.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	auto char *config;
+
+	auto int retn = 1;
+
+	auto Buffer bufr = NULL;
+
+	auto Config parser = NULL;
+
+	auto Duct duct = NULL;
+
+	auto IDtoken token = NULL;
+
+	auto Authenticator authn = NULL;
+
+
+	/* Get the organizational identifier and SSN. */
+	while ( (retn = getopt(argc, argv, "c:")) != EOF )
+		switch ( retn ) {
+
+			case 'c':
+				config = optarg;
+				break;
+		}
+	retn = 1;
+
+	if ( config == NULL )
+		config = "./query-client.conf";
+
+
+	if ( (bufr = HurdLib_Buffer_Init()) == NULL ) {
+		fputs("Cannot initialize receive buffer.\n", stderr);
+		goto done;
+	}
+
+	/* Initialize SSL connection and wait for connections. */
+	if ( (duct = NAAAIM_Duct_Init()) == NULL ) {
+		fputs("Error on SSL object creation.\n", stderr);
+		goto done;
+	}
+
+	if ( !duct->init_client(duct) ) {
+		fputs("Cannot initialize server mode.\n", stderr);
+		goto done;
+	}
+
+	if ( !duct->load_certificates(duct, "./org-cert.pem") ) {
+		fputs("Cannot load certificates.\n", stderr);
+		goto done;
+	}
+
+	if ( !duct->init_port(duct, "localhost", 11990) ) {
+		fputs("Cannot initialize port.\n", stderr);
+		goto done;
+	}
+
+	if ( !duct->init_connection(duct) ) {
+		fputs("Cannot initialize connection.\n", stderr);
+		goto done;
+	}
+
+
+	/* Obtain connection banner. */
+	if ( !duct->receive_Buffer(duct, bufr) ) {
+		fputs("Error on receive.\n", stderr);
+		goto done;
+	}
+	bufr->add(bufr, (unsigned char *) "\0", sizeof(1));
+	fprintf(stdout, "%s", bufr->get(bufr));
+
+
+	/* Initialize an authenticator object. */
+	if ( (authn = NAAAIM_Authenticator_Init()) == NULL ) {
+		fputs("Cannot initialize authenticator.\n", stderr);
+		goto done;
+	}
+
+
+	/* Initialize and load device identity. */
+	if ( (token = NAAAIM_IDtoken_Init()) == NULL ) {
+		fputs("Cannot initialize identity token.\n", stderr);
+		goto done;
+	}
+	if ( !load_identity(token, "./device1.txt") ) {
+		fputs("Cannot load device identity.\n", stderr);
+		goto done;
+	}
+
+	if ( !authn->add_identity(authn, token) ) {
+		fputs("Cannot add device identity.\n", stderr);
+		goto done;
+	}
+
+
+	token->reset(token);
+	if ( !load_identity(token, "./user1.txt") ) {
+		fputs("Cannot load user identity.\n", stderr);
+		goto done;
+	}
+
+	authn->add_element(authn, token->get_element(token, IDtoken_orgkey));
+	authn->encrypt(authn, "./org-private.pem");
+
+	bufr->reset(bufr);
+	if ( !authn->encode(authn, bufr) ) {
+		fputs("Error encoding device authenticator.\n", stderr);
+		goto done;
+	}
+
+	if ( !duct->send_Buffer(duct, bufr) )
+		fputs("Error transmitting device authenticator.\n", stderr);
+	authn->print(authn);
+
+
+ done:
+	if ( !duct->whack_connection(duct) )
+		fputs("Error closing connection.\n", stderr);
+
+	if ( parser != NULL )
+		parser->whack(parser);
+	if ( duct != NULL )
+		duct->whack(duct);
+	if ( bufr != NULL )
+		bufr->whack(bufr);
+	if ( token != NULL )
+		token->whack(token);
+	if ( authn != NULL )
+		authn->whack(authn);
+
+	return retn;
+}
---
+++ root-referral.c
@@ -0,0 +1,289 @@
+/** \file
+ * This file contains an implementation of the identity root referral
+ * server.   This server is responsible for arbitrating all medical
+ * identity referral requests.
+ *
+ * The server accepts an identity referral request which consists of
+ * two authenticators.  The first authenticator identitifies the
+ * device and contains an encrypted list of organizational modifier 
+ * keys.  The second authenticator identifies the user and contains an
+ * encrypted list of organizational identifiers.
+ *
+ * The server tranfers the two authenticators to the user and device
+ * identity brokerage servers.  If these servers successfully authenticate
+ * the device and user the decrypted organizational modifier keys and
+ * identities are returned.
+ *
+ * The server then transfers the modifier and identity blocks to the
+ * identity brokerage servers for discovery.  The identity query
+ * responses are collected from the brokerage servers and returned to the
+ * initiating client.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2010, Enjellic Systems Development. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+
+/* Local defines. */
+#define SITE "Gardonville Cooperative Telephone"
+#define LOCATION "Brandon, MN"
+
+/* Include files. */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <Config.h>
+#include <Buffer.h>
+
+#include "NAAAIM.h"
+#include "Duct.h"
+#include "IDtoken.h"
+#include "Authenticator.h"
+
+
+/* Variables static to this module. */
+static pid_t process_table[100];
+
+
+/**
+ * Private function.
+ *
+ * This function initializes the process table.
+ */
+
+static void init_process_table(void)
+
+{
+	auto unsigned int lp;
+
+
+	for (lp= 0; lp < sizeof(process_table)/sizeof(pid_t); ++lp)
+		process_table[lp] = 0;
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function adds an entry to the process state table.  It will
+ * locate an empy slot in the table and place the PID of the dispatched
+ * process in that slot.
+ *
+ * \param pid	The process ID to be placed in the table.
+ */
+
+static void add_process(pid_t pid)
+
+{
+	auto unsigned int lp;
+
+
+	for (lp= 0; lp < sizeof(process_table)/sizeof(pid_t); ++lp)
+		if ( process_table[lp] == 0 ) {
+			process_table[lp] = pid;
+			return;
+		}
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function reaps any available processes and updates its slot in
+ * the process table.
+ */
+
+static void update_process_table(void)
+
+{
+	auto unsigned int lp;
+
+	auto int pid,
+		 status;
+
+
+	while ( (pid = waitpid(-1, &status, WNOHANG)) > 0 )
+		for (lp= 0; lp < sizeof(process_table)/sizeof(pid_t); ++lp)
+			if ( process_table[lp] == pid ) {
+				process_table[lp] = 0;
+				fprintf(stdout, "%d terminated", pid);
+				if ( !WIFEXITED(status) ) {
+					fputs(" abnormally\n", stdout);
+					continue;
+				}
+				fprintf(stdout, ", status = %d\n", \
+					WEXITSTATUS(status));
+			}
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function is called after a fork to handle an accepted connection.
+ *
+ * \param duct	The SSL connection object describing the accepted connection.
+ *
+ * \return	A value of zero is used to indicate the connection has
+ *		been handled successfully.  A value of 1 indicates
+ *		connection handling has failed.
+ */
+
+static int handle_connection(const Duct const duct)
+
+{
+	auto char banner[256];
+
+	auto int retn = 1;
+
+	auto Buffer bufr = NULL;
+
+	auto Authenticator authn = NULL;
+
+
+	if ( (bufr = HurdLib_Buffer_Init()) == NULL )
+		goto done;
+	if ( (authn = NAAAIM_Authenticator_Init()) == NULL )
+		goto done;
+		
+
+	/* Send the connection banner. */
+	snprintf(banner, sizeof(banner), \
+		 "Hello\nRoot Referral Server / %s / %s\n", SITE, LOCATION);
+	bufr->add(bufr, (unsigned char *) banner, strlen(banner));
+	if ( !duct->send_Buffer(duct, bufr) )
+		goto done;
+
+
+	/* Read and process device authenticator. */
+	bufr->reset(bufr);
+	if ( !duct->receive_Buffer(duct, bufr) )
+		goto done;
+	fputs("Received:\n", stdout);
+	bufr->print(bufr);
+
+	fputs("Doing decode.\n", stdout);
+	if ( !authn->decode(authn, bufr) ) {
+		fputs("Failed decode.\n", stderr);
+		goto done;
+	}
+
+	fputs("Device authenticator.\n", stdout);
+	authn->print(authn);
+	
+	retn = 0;
+
+
+ done:
+	if ( bufr != NULL )
+		bufr->whack(bufr);
+	if ( authn != NULL )
+		authn->whack(authn);
+	return retn;
+}
+
+
+/*
+ * Program entry point begins here.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	auto char *config;
+
+	auto int retn = 1;
+
+	auto pid_t pid;
+
+	auto Config parser = NULL;
+
+	auto Duct duct = NULL;
+
+
+	/* Get the organizational identifier and SSN. */
+	while ( (retn = getopt(argc, argv, "c:")) != EOF )
+		switch ( retn ) {
+
+			case 'c':
+				config = optarg;
+				break;
+		}
+	retn = 1;
+
+	if ( config == NULL )
+		config = "./root-referral.conf";
+
+
+	/* Initialize process table. */
+	init_process_table();
+
+
+	/* Initialize SSL connection and wait for connections. */
+	if ( (duct = NAAAIM_Duct_Init()) == NULL ) {
+		fputs("Error on SSL object creation.\n", stderr);
+		goto done;
+	}
+
+	if ( !duct->init_server(duct) ) {
+		fputs("Cannot initialize server mode.\n", stderr);
+		goto done;
+	}
+
+	if ( !duct->load_credentials(duct, "./org-private.pem", \
+				     "./org-cert.pem") ) {
+		fputs("Cannot load server credentials.\n", stderr);
+		goto done;
+	}
+
+	if ( !duct->init_port(duct, NULL, 11990) ) {
+		fputs("Cannot initialize port.\n", stderr);
+		goto done;
+	}
+
+	while ( 1 ) {
+		if ( !duct->accept_connection(duct) ) {
+			fputs("Error on SSL connection accept.\n", stderr);
+			goto done;
+		}
+
+		pid = fork();
+		if ( pid == -1 ) {
+			fputs("Connection fork failure.\n", stderr);
+			goto done;
+		}
+		if ( pid == 0 ) {
+			retn = handle_connection(duct);
+			goto done;
+		}
+
+		fprintf(stdout, "Client connection dispatched to: %d\n", \
+			pid);
+		add_process(pid);
+		update_process_table();
+		duct->reset(duct);
+	}
+
+
+ done:
+	if ( !duct->whack_connection(duct) )
+		fputs("Error closing connection.\n", stderr);
+
+	if ( parser != NULL )
+		parser->whack(parser);
+	if ( duct != NULL )
+		duct->whack(duct);
+
+	return retn;
+}
