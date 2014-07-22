8d5abefebf423adffa8595e904cadeeeb48c9296
Dr. Greg Wettstein <greg@idfusion.org>
2014-07-22 03:57:16 -0500

    Initial implementation of init.
    This is a commit designed to introduce the framework for an init
    implemenation.  The init daemon will be spawned from the boot
    daemon after population of the root filesytem.

---
+++ utils/init.c
@@ -0,0 +1,33 @@
+/** \file
+ */
+
+/**************************************************************************
+ * (C)Copyright 2014, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+
+/* Local defines. */
+
+
+/* Include files. */
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <HurdLib.h>
+
+     
+/*
+ * Program entry point begins here.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	fputs("Init started.\n", stdout);
+	return 0;
+}
