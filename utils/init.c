eab2634389f58b2a59691d8ad7b93dc687868287
Dr. Greg Wettstein <greg@idfusion.org>
2014-07-29 11:42:07 -0500

    Implement loading of initial system configuration.
    The initial implementation of the init daemon is extended to
    include the reading of the system configuration information.
    This is done after dropping the root identity of the process in
    order to prevent the configuration information from changing the
    measurement status of the system.
    
    The init daemon uses this configuration information to set the
    eth0 network address.  The mode of the system is also set which
    will be used to customize further configurations.

--- utils/init.c
+++ utils/init.c
@@ -1,4 +1,11 @@
 /** \file
+ * This file implements a system manager daemon.  It is designed to
+ * play a very similar role to the classic 'init' daemon in multi-user
+ * systems.
+ *
+ * In the context of the secure platform model this daemon is invoked
+ * by being exec'ed by the boot daemon from the decrypted master
+ * filesystem image.
  */
 
 /**************************************************************************
@@ -17,10 +24,135 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/reboot.h>
+#include <sys/mount.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include <HurdLib.h>
+#include <Config.h>
 
-     
+#include "Netconfig.h"
+
+
+/* Definitions static to this module. */
+static Config Cfg = NULL;
+
+static char *Mode = NULL;
+
+
+/**
+ * Private function.
+ *
+ * This function is responsible for terminating the boot process.  It
+ * requests a reboot of the system.
+ *
+ * No arguements are expected by this function.
+ *
+ * This function does not return.
+ */
+
+static void do_reboot(void)
+
+{
+	fputs("Reboot requested.\n", stderr);
+	reboot(RB_HALT_SYSTEM);
+	return;
+
+#if 0
+	fputs("Rebooting.\n", stderr);
+	reboot(RB_AUTOBOOT);
+#endif
+}
+
+
+/**
+ * Private function.
+ *
+ * This function is responsible for loading the init configuration into
+ * the statically scoped Cfg variable.
+ *
+ * No arguements are expected by this function.
+ *
+ * \return	If an error is encountered while loading the configuration
+ *		a false value is returned.  A true value indicates the
+ *		configuration was successfully loaded.
+ */
+
+static _Bool load_config(void)
+
+{
+	_Bool retn = false;
+
+
+	INIT(HurdLib, Config, Cfg, goto done);
+	
+	if ( setreuid(1, -1) == -1 )
+		goto done;
+	if ( !Cfg->parse(Cfg, "/etc/conf/init.conf") )
+		goto done;
+	if ( setreuid(geteuid(), -1) == -1 )
+		goto done;
+
+	if ( (Mode = Cfg->get(Cfg, "mode")) == NULL )
+		goto done;
+	retn = true;
+
+ done:
+	return retn;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function is responsible for setting up the base network
+ * configuration for the platform.  The network configuration is
+ * obtained from the global Cfg variable.
+ *
+ * No arguements are expected by this function.
+ *
+ * \return	If an error is encountered while configuring the
+ *		network a false value is returned.  A true value indicates
+ *		the network was successfully configured.
+ */
+
+static _Bool configure_network(void)
+
+{
+	_Bool retn = false;
+
+	char *iface,
+	     *address,
+	     *mask;
+
+	Netconfig netconfig = NULL;
+
+
+	INIT(NAAAIM, Netconfig, netconfig, goto done);
+	if ( !netconfig->set_address(netconfig, "lo", "127.0.0.1", \
+				     "255.0.0.0") )
+		goto done;
+	WHACK(netconfig);
+
+	iface   = Cfg->get(Cfg, "interface");
+	address = Cfg->get(Cfg, "address");
+	mask	= Cfg->get(Cfg, "mask");
+	if ( (iface == NULL) || (address == NULL) || (mask == NULL) )
+		goto done;
+
+	INIT(NAAAIM, Netconfig, netconfig, goto done);
+	if ( !netconfig->set_address(netconfig, iface, address, mask) )
+		goto done;
+	retn = true;
+
+
+ done:
+	WHACK(netconfig);
+	return retn;
+}
+	
+
 /*
  * Program entry point begins here.
  */
@@ -28,6 +160,21 @@
 extern int main(int argc, char *argv[])
 
 {
-	fputs("Init started.\n", stdout);
-	return 0;
+	int retn = 1;
+
+
+	if ( !load_config() )
+		goto done;
+
+	if ( !configure_network() )
+		goto done;
+
+	fprintf(stdout, "%s: OK\n", Mode);
+	execl("/bin/sh", "/bin/sh", NULL);
+
+
+ done:
+	WHACK(Cfg);
+	do_reboot();
+	return retn;
 }
