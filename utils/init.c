3d27914890e0c8a8b541c8105933f99a423e1b8a
Dr. Greg Wettstein <greg@idfusion.org>
2014-10-29 03:45:52 -0500

    Update init for hardware environment.
    The init utility was not starting the tscd daemon if running in a
    hardware TPM environment.  Start the daemon if a software RPM
    environment was not specified.
    
    Add support for starting the identity generation engine if init
    is running as HUI.

--- utils/init.c
+++ utils/init.c
@@ -17,8 +17,14 @@
 
 
 /* Local defines. */
+/* Measurement UID. */
+#define MEASUREMENT_UID 1
+
 /* UID of which identity manager is to run under. */
 #define IDMGR_UID 1
+
+/* TPM daemon location. */
+#define TCSD_PATH "/usr/local/sbin/tcsd"
 
 
 /* Include files. */
@@ -97,8 +103,10 @@ static void do_reboot(void)
 static _Bool start_tpm(void)
 
 {
-	_Bool software_tpm = false,
-	      retn = false;
+	_Bool retn	   = false,
+	      software_tpm = false;
+
+	pid_t tpm_pid;
 
 	Buffer b;
 
@@ -107,23 +115,47 @@ static _Bool start_tpm(void)
 	SoftwareStatus software = NULL;
 
 
+	/* Start either the software TPM stack or just the hardware stack. */
 	software_tpm = (Cfg->get(Cfg, "tpm") != NULL);
 	if ( software_tpm )
 		software_tpm = (strcmp(Cfg->get(Cfg, "tpm"), "software") == 0);
+
 	if ( software_tpm ) {
 		INIT(NAAAIM, SoftwareTPM, SWtpm, goto done);
 		if ( !SWtpm->start(SWtpm, 1) )
 			goto done;
+	} else {
+		tpm_pid = fork();
+		if ( tpm_pid == -1 )
+			goto done;
+		/* Child. */
+		if ( tpm_pid == 0 ) {
+			execl(TCSD_PATH, TCSD_PATH, "-f", NULL);
+			goto done;
+		}
+
+		/* Parent - verify DAEMON is running. */
+		fputs("Checking for daemon presence.\n", stderr);
+		sleep(5);
+		if ( kill(tpm_pid, 0) == -1 )
+			goto done;
 	}
 
-	INIT(NAAAIM, TPMcmd, tpm, goto done);
-	INIT(NAAAIM, SoftwareStatus, software, goto done);
-	if ( !software->open(software) )
-		goto done;
-	if ( !software->measure(software) )
-		goto done;
 
+	/*
+	 * Generate a system software measurement if software TPM is
+	 * being used.  This is done by default for a hardware TPM
+	 * by the kernel.
+	 */
 	if ( software_tpm ) {
+		INIT(NAAAIM, TPMcmd, tpm, goto done);
+		INIT(NAAAIM, SoftwareStatus, software, goto done);
+
+		if ( !software->open(software) )
+			goto done;
+		if ( !software->measure(software) )
+			goto done;
+
 		b = software->get_template_hash(software);
 		tpm->pcr_extend(tpm, 10, b);
 		fputs("software tpm: Extended software status: ", stdout);
@@ -297,6 +329,70 @@ static _Bool start_identity_manager(void)
 /**
  * Private function.
  *
+ * This function is responsible for checking whether or not a system
+ * measurement should be executed.  A system measurement is requested by
+ * placing a signed measurement request in the system boot directory.
+ *
+ * No arguements are expected by this function.
+ *
+ * \return	The return value of this system is used to indicate
+ *		whether or not the system should proceed forward.  If
+ *		a measurement is requested and/or succeeds a false value
+ *		is returned.   true value is used to indicate the system
+ *		should proceed forward with initialization.
+ */
+
+static _Bool measure_system(void)
+
+{
+	_Bool retn = true;
+
+	int status;
+
+	const char * const measurement = "/mnt/boot/measurement";
+	const char * const verifier    = "/usr/local/sbin/gen-id-verifier";
+
+	pid_t pid;
+
+	struct stat measurement_stat;
+
+
+	/* Check for presence of measurement request. */
+	if ( stat(measurement, &measurement_stat) != 0 ) 
+		goto done;
+
+	/* Generate the system measurement. */
+	if ( (pid = fork()) == -1 )
+		goto done;
+
+	/* Child. */
+	if ( pid == 0 ) {
+		if ( chdir(measurement) != 0 )
+			goto done;
+		execl(verifier, verifier, "-m", "-f", "-k",	 \
+		      "/etc/conf/pubkey", "-u", "/etc/conf/aik", \
+		      (char *) NULL);
+		goto done;
+	}
+
+	/* Parent - wait for verification to complete. */
+	if ( waitpid(pid, &status, 0) == -1 )
+		goto done;
+	fputs("System measurement generated - rebooting.\n", stderr);
+	sync();
+	sleep(2);
+	retn = false;
+
+
+ done:
+	chdir("/");
+	return retn;
+}
+
+	
+/**
+ * Private function.
+ *
  * This function is responsible for configuring the internal IPsec tunnel
  * used to transport commands between the two nodes.  The static Mode
  * variable is used to specify the personality type for the connection.
@@ -366,20 +462,31 @@ extern int main(int argc, char *argv[])
 	if ( !start_identity_manager() )
 		goto done;
 
+	fputs("Checking system measurement request.\n", stderr);
+	if ( !measure_system() )
+		goto done;
+
 	fputs("Starting internal tunnel.\n", stderr);
-	configure_internal_tunnel();
-#if 0
 	if ( !configure_internal_tunnel() )
 		goto done;
-#endif
 
-	fprintf(stdout, "%s: OK\n", Mode);
-	execl("/bin/sh", "/bin/sh", NULL);
+	if ( strcmp(Mode, "hui") == 0 ) {
+		fputs("Starting identity engine.\n", stderr);
+		system("/usr/local/sbin/idgine &");
+		sleep(5);
+		fputs("Starting identity server.\n", stderr);
+		execl("/usr/local/sbin/idsvr", "/usr/local/sbin/idsvr", \
+		      "-h", "10.0.2.1", NULL);
+	} else {
+		fprintf(stdout, "%s: OK\n", Mode);
+		execl("/bin/sh", "/bin/sh", NULL);
+	}
 
 
  done:
 	WHACK(Cfg);
 	WHACK(SWtpm);
+
 	do_reboot();
 	return retn;
 }
