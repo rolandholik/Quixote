e8a64455a37ffb6f11617419c2785d32835494d7
Dr. Greg Wettstein <gw@idfusion.org>
2016-02-18 16:34:39 -0600

    Implement utility to copy monitor EDID blocks.
    The iGVT (Intel Graphics Virtualization) infrastructure requires
    support for 'hot-plugging' the monitor when a graphics
    virtualization instance is created.  This requires copying the
    128 byte Extended Display Identification Data (EDID) data block
    from the system monitor to the connector instance for the
    virtualized graphics adapter.
    
    This very simplistic utility is designed to replace the use of dd
    which Intel supplied scripts use.

---
+++ utils/copy-edid.c
@@ -0,0 +1,80 @@
+/** \file
+ * This file implements a utility for copying an EDID block from
+ * a pseudo-file source to a destination pseudo-file.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2016, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+
+/* Local defines. */
+#define EDID_SIZE 128
+
+
+/* Include files. */
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <HurdLib.h>
+#include <Buffer.h>
+#include <String.h>
+#include <File.h>
+
+
+/*
+ * Program entry point begins here.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	_Bool retn = false;
+
+	Buffer bufr = NULL;
+
+	File input  = NULL,
+	     output = NULL;
+
+
+	if ( argc != 3 ) {
+		fputs("Improper number of arguements.\n", stderr);
+		return 1;
+	}
+
+	INIT(HurdLib, Buffer, bufr, goto done);
+
+	INIT(HurdLib, File, input, goto done);
+	INIT(HurdLib, File, output, goto done);
+
+	if ( !input->open_ro(input, argv[1]) )
+		goto done;
+	if ( !output->open_wo(output, argv[2]) )
+		goto done;
+
+	if ( !input->read_Buffer(input, bufr, EDID_SIZE) )
+		goto done;
+	if ( !output->write_Buffer(output, bufr) )
+		goto done;
+
+	retn = true;
+
+
+ done:
+	WHACK(bufr);
+	WHACK(input);
+	WHACK(output);
+
+	if ( !retn )
+		fputs("EDID copy error\n", stderr);
+
+	return retn ? 0 : 1;
+}
