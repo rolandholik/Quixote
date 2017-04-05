c14aa592b10b6ccf9dc6fc6d77fa2ed5284c53d5
Dr. Greg Wettstein <gw@idfusion.org>
2017-04-04 20:16:02 -0500

    Implement SHA hashing tool.
    This update introduces the sha-tool utility which provides
    support for implementing SHA256 hashing of various data items
    important for the computation of iso-identity system
    measurements.
    
    There are currently three separate options supported:
    
    - Computing the SHA256 of a filename or other arbitrary string.
    
    - Computing the SHA256 of the contents of a file.
    
    - Generate a 'null' SHA256 value, ie. all zeros.

---
+++ ISO-identity/sha-tool.c
@@ -0,0 +1,117 @@
+/** \file
+ * This file implements a general purpose tool for computing digests
+ * of values which are important in system trajectory measurements.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2017, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+
+/* Include files. */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <HurdLib.h>
+#include <Buffer.h>
+#include <String.h>
+#include <File.h>
+
+#include <NAAAIM.h>
+#include <SHA256.h>
+
+
+/*
+ * Program entry point begins here.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	_Bool null = false;
+
+	int opt,
+	    retn = 1;
+
+	char *infile = NULL,
+	     *string = NULL;
+
+	Buffer bufr = NULL;
+
+	SHA256 sha256 = NULL;
+
+	File file = NULL;
+
+
+	/* Parse and verify arguements. */
+	while ( (opt = getopt(argc, argv, "nf:s:")) != EOF )
+		switch ( opt ) {
+			case 'n':
+				null = true;
+			
+			case 'f':
+				infile = optarg;
+				break;
+			
+			case 's':
+				string = optarg;
+				break;
+		}
+
+
+	/* Generate a null hash. */
+	if ( null ) {
+		uint8_t lp;
+
+		INIT(HurdLib, Buffer, bufr, ERR(goto done));
+
+		for (lp= 0; lp < 32; ++lp)
+			if ( !bufr->add(bufr, (unsigned char *) "\0", 1) )
+				ERR(goto done);
+		bufr->print(bufr);
+	}
+
+
+	/* Compute value of string. */
+	if ( string != NULL ) {
+		INIT(HurdLib, Buffer, bufr, ERR(goto done));
+		bufr->add(bufr, (unsigned char *) string, strlen(string));
+
+		INIT(NAAAIM, SHA256, sha256, ERR(goto done));
+		sha256->add(sha256, bufr);
+		if ( !sha256->compute(sha256) )
+			ERR(goto done);
+		sha256->print(sha256);
+	}
+
+	/* Compute value of file. */
+	if ( infile != NULL ) {
+		INIT(HurdLib, Buffer, bufr, ERR(goto done));
+
+		INIT(HurdLib, File, file, ERR(goto done));
+		file->open_ro(file, infile);
+		if ( !file->slurp(file, bufr) )
+			ERR(goto done);
+
+		INIT(NAAAIM, SHA256, sha256, ERR(goto done));
+		sha256->add(sha256, bufr);
+		if ( !sha256->compute(sha256) )
+			ERR(goto done);
+		sha256->print(sha256);
+	}
+		
+
+ done:
+	WHACK(bufr);
+	WHACK(file);
+	WHACK(sha256);
+
+	return retn;
+}
