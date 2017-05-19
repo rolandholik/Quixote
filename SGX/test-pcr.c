225e4714e668a1354511555ac70d8055ed3cd25c
Dr. Greg Wettstein <gw@idfusion.org>
2017-05-19 16:35:26 -0500

    Implement test harness for SGX based measurement enclave.
    This update introduces an SGX enclave which is used to implement
    SHA256 based measurement registers.  This enclave is an initial
    proof of concept implementation which is expected to be enhanced
    to support multiple registers.
    
    The enclave currently supports the ability to accept an arbitrary
    buffer and extended the current measurement state with the
    measurement of that buffer.  The current implementation arguably
    needs to be modified to first reduce the buffer to a 256 bit
    measurement and use that binary value to extend the current 256
    bit measurement.
    
    The enclave also supports the ability to read the current
    measurement value from the enclave.
    
    This update also introduces the test-pcr utility which exercises
    the functionality of the enclave.  The utility currently executes
    a loop which requests input from the user and passes the string
    typed by the user into the enclave to be used as the measurement
    extension.

---
+++ SGX/test-pcr.c
@@ -0,0 +1,215 @@
+/** \file
+ * This file contains an implemenation of simple enclave execution with
+ * an OCALL handler which implements printing a arbitrary string from
+ * inside the enclave.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2016, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+
+/* Include files. */
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <Origin.h>
+#include <HurdLib.h>
+#include <String.h>
+#include <Buffer.h>
+#include <File.h>
+
+#include "NAAAIM.h"
+#include "SGX.h"
+#include "SGXenclave.h"
+
+
+/* Define the OCALL interface for the 'print string' call. */
+struct ocall1_interface {
+	char* str;
+} ocall1_string;
+
+int ocall1_handler(struct ocall1_interface *interface)
+
+{
+	fprintf(stdout, "%s", interface->str);
+	return 0;
+}
+
+static const struct OCALL_api ocall_table = {
+	1, {ocall1_handler}
+};
+
+static struct ecall0_table {
+	uint8_t *buffer;
+	size_t len;
+} ecall0_table;
+
+static struct ecall1_table {
+	uint8_t *buffer;
+	size_t len;
+} ecall1_table;
+
+
+static _Bool enclave_loop(CO(SGXenclave, enclave))
+
+{
+	_Bool retn = false;
+
+	uint8_t lp;
+
+	int rc;
+
+	uint8_t inbufr[1024];
+
+
+	while ( true ) {
+		fputs("Input>", stdout);
+		fflush(stdout);
+
+		if ( fgets((char *) inbufr, sizeof(inbufr), stdin) == NULL ) {
+			fputc('\n', stdout);
+			retn = true;
+			goto done;
+		}
+
+		ecall0_table.len    = strlen((char *) inbufr);
+		ecall0_table.buffer = (uint8_t *) inbufr;
+
+		if ( !enclave->boot_slot(enclave, 0, &ocall_table, \
+					 &ecall0_table, &rc) ) {
+			fprintf(stderr, "Enclave returned: %d\n", rc);
+			goto done;
+		}
+
+		memset(inbufr, '\0', sizeof(inbufr));
+		ecall1_table.len    = sizeof(inbufr);
+		ecall1_table.buffer = inbufr;
+		if ( !enclave->boot_slot(enclave, 1, &ocall_table, \
+					 &ecall1_table, &rc) ) {
+			fprintf(stderr, "Enclave returned: %d\n", rc);
+			goto done;
+		}
+
+		fputs("PCR:\n", stdout);
+		for (lp= 0; lp < 32; ++lp)
+			fprintf(stdout, "%02x", inbufr[lp]);
+		fputc('\n', stdout);
+	}
+
+
+ done:
+	return retn;
+}
+
+
+/**
+ * Program entry point.
+ *
+ * The following arguements are processed:
+ *
+ *	-d:	By default the debug attribute is set for the enclave.
+ *		This option toggles that option.
+ *
+ *	-e:	The enclave which is to be executed.
+ *
+ *	-n:	The SGX device node to be used.  By default /dev/isgx.
+ *
+ *	-t:	The file containing the EINITTOKEN for this processor.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	_Bool debug = true;
+
+	char *token	   = NULL,
+	     *sgx_device   = "/dev/isgx",
+	     *enclave_name = NULL;
+
+	int opt,
+	    retn = 1;
+
+	struct SGX_einittoken *einit;
+
+	SGXenclave enclave = NULL;
+
+	Buffer bufr = NULL;
+
+	File token_file = NULL;
+
+
+	/* Parse and verify arguements. */
+	while ( (opt = getopt(argc, argv, "dne:t:")) != EOF )
+		switch ( opt ) {
+			case 'd':
+				debug = debug ? false : true;
+			case 'e':
+				enclave_name = optarg;
+				break;
+			case 'n':
+				sgx_device = optarg;
+				break;
+			case 't':
+				token = optarg;
+				break;
+		}
+
+	if ( enclave_name == NULL ) {
+		fputs("No enclave name specifed.\n", stderr);
+		goto done;
+	}
+
+
+	/* Load the launch token. */
+	if ( token == NULL ) {
+		fputs("No EINIT token specified.\n", stderr);
+		goto done;
+	}
+
+	INIT(HurdLib, Buffer, bufr, ERR(goto done));
+	INIT(HurdLib, File, token_file, ERR(goto done));
+
+	token_file->open_ro(token_file, token);
+	if ( !token_file->slurp(token_file, bufr) )
+		ERR(goto done);
+	einit = (void *) bufr->get(bufr);
+
+
+	/* Load an initialize the enclave. */
+	INIT(NAAAIM, SGXenclave, enclave, ERR(goto done));
+
+	if ( !enclave->open_enclave(enclave, sgx_device, enclave_name, debug) )
+		ERR(goto done);
+
+	if ( !enclave->create_enclave(enclave) )
+		ERR(goto done);
+
+	if ( !enclave->load_enclave(enclave) )
+		ERR(goto done);
+
+	if ( !enclave->init_enclave(enclave, einit) )
+		ERR(goto done);
+
+	if ( !enclave_loop(enclave) )
+		ERR(goto done);
+
+	retn = 0;
+
+
+ done:
+	WHACK(bufr);
+	WHACK(token_file);
+	WHACK(enclave);
+
+	return retn;
+
+}
