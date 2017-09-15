71d95e2f35a37f2f8b6610139a0ea950f6e4db78
Dr. Greg Wettstein <gw@idfusion.org>
2017-09-15 15:00:43 -0500

    Add missing include file.
    Unfortunately the previous commit will not yield a compilable
    build since it was missing the cboot.h file which contains
    definitions and declarations which are common between the
    canister boot and management daemons.  This commit adds the
    missing file.

---
+++ utils/cboot.h
@@ -0,0 +1,32 @@
+/** \file
+ *
+ * This file implements the definitions for the command interface 
+ * for a cboot management instance.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2017, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+/* Basename for the management socket. */
+#define SOCKNAME "/var/run/cboot-mgmt"
+
+
+/* Command definitions. */
+enum {
+	show_measurement=1
+} cboot_commands;
+
+struct cboot_cmd_definition {
+	int command;
+	char *syntax;
+};
+
+struct cboot_cmd_definition cboot_cmd_list[] = {
+	{show_measurement, "show measurement"},
+	{0, NULL}
+};
+	
