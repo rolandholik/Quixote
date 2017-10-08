3faa56559a16070a856e4683740e5841d557ef07
Dr. Greg Wettstein <gw@idfusion.org>
2017-10-08 10:27:42 -0500

    Implement a utility to compute aggregate measurement values.
    This update implements a simple utility program named
    compute-aggregate which can be used to generate the SHA256
    aggregate value which is generated by the kernel IMA
    implementation.  The utility takes two arguements which are the
    actor and subject identities from the first line in the following
    pseudo-file:
    
    /sys/kernel/security/ima/ascii_runtime_measurements
    
    The 256 bit value generated will be the measurement which is at
    the root of the measurement list either for the kernel
    implementation or a userspace simulation of the kernel
    implementation.

---
+++ ISO-identity/compute-aggregate.c
@@ -0,0 +1,116 @@
+/** \file
+ * This file implements the computation of the boot aggregate value
+ * which is the initial measurement extended into the TPM.  The
+ * values used to compute the aggregate value are derived from the
+ * actor and subject values from the first line in the following
+ * pseudo-file:
+ *
+ *	/sys/kernel/security/ima/ascii_runtime_measurements
+ */
+
+/**************************************************************************
+ * (C)Copyright 2017, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+
+/* Include files. */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <HurdLib.h>
+#include <Buffer.h>
+
+#include <NAAAIM.h>
+#include <SHA256.h>
+
+
+/*
+ * Program entry point begins here.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	char *actor   = NULL,
+	     *subject = NULL;
+
+	int opt,
+	    retn = 1;
+
+	uint32_t length = NAAAIM_IDSIZE;
+
+	Buffer bufr = NULL;
+
+	SHA256 sha256 = NULL;
+
+
+	/* Parse and verify arguements. */
+	while ( (opt = getopt(argc, argv, "a:s:")) != EOF )
+		switch ( opt ) {
+			case 'a':
+				actor = optarg;
+				break;
+			case 's':
+				subject = optarg;
+				break;
+		}
+
+
+	/* Validate actor and subject aggregate values. */
+	if ( actor == NULL ) {
+		fputs("No actor aggregate value.\n", stderr);
+		goto done;
+	}
+	if ( strlen(actor) != NAAAIM_IDSIZE*2 ) {
+		fputs("Invalid actor aggregate value specified.\n", stderr);
+		goto done;
+	}
+
+	if ( subject == NULL ) {
+		fputs("No subject aggregate value.\n", stderr);
+		goto done;
+	}
+	if ( strlen(subject) != NAAAIM_IDSIZE*2 ) {
+		fputs("Invalid subject aggregate value specified.\n", stderr);
+		goto done;
+	}
+
+
+	/* Initialize objects. */
+	INIT(HurdLib, Buffer, bufr, ERR(goto done));
+	INIT(NAAAIM, SHA256, sha256, ERR(goto done));
+
+
+	/* Add the length and actor value. */
+	if ( !bufr->add(bufr, (unsigned char * ) &length, sizeof(length)) )
+		ERR(goto done);
+	if ( !bufr->add_hexstring(bufr, actor) )
+		ERR(goto done);
+
+	if ( !bufr->add(bufr, (unsigned char * ) &length, sizeof(length)) )
+		ERR(goto done);
+	if ( !bufr->add_hexstring(bufr, subject) )
+		ERR(goto done);
+
+
+	/* Compute the measurement over the measurement buffer. */
+	sha256->add(sha256, bufr);
+	if ( !sha256->compute(sha256) )
+		ERR(goto done);
+
+	sha256->print(sha256);
+	retn = 0;
+
+ done:
+	WHACK(bufr);
+	WHACK(sha256);
+
+	return retn;
+}
