a5582fd0b3ef6062604f7b2e30ba0cdfe645e4b5
Dr. Greg Wettstein <gw@idfusion.org>
2017-10-14 10:16:45 -0500

    Add test program for ExchangeEvent object.
    The test-event utility was added as a unit test for the
    ExchangeEvent object.  It parses an internally supplied
    information exchange event screen and outputs the component
    identity information about the event.
    
    The utility regenerates the event string with the
    ExchangeEvent->format method and then re-parses that string to
    verify that the measurement of the event is consistent with that
    produced by the test string.  This validates the fidelity of the
    text parsing and inversion process.

---
+++ ISO-identity/test-event.c
@@ -0,0 +1,105 @@
+/** \file
+ * This file implements a test driver for the InformationExchange object
+ * which is model an information exchange event in the iso-identity
+ * behavior modeling system.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2017, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ **************************************************************************/
+
+
+/* Include files. */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+
+#include <HurdLib.h>
+#include <Buffer.h>
+#include <String.h>
+#include <File.h>
+
+#include <NAAAIM.h>
+
+#include "ExchangeEvent.h"
+
+
+/*
+
+ * Program entry point begins here.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	int retn = 1;
+
+	String entry = NULL;
+
+	Buffer bufr = NULL;
+
+	ExchangeEvent event = NULL;
+
+
+	INIT(HurdLib, Buffer, bufr, ERR(goto done));
+
+	INIT(HurdLib, String, entry, ERR(goto done));
+	if ( !entry->add(entry, "event{swapper/0:/bin/bash-3.2.48} actor{uid=0, euid=0, suid=0, gid=0, egid=0, sgid=0, fsuid=0, fsgid=0, cap=3fffffffff} subject{uid=0, gid=0, mode=o100755, name_length=16, name=e1cb9766d47adb4d514d5590dd247504a3aab7e67839d65a6c6f4c32fc120e5d, s_id=xvda, s_uuid=feadbeaffeadbeaffeadbeaffeadbeaf, digest=d2a6bfe0d8a2346d45518dcaaf47642808d6c605506bd0b8e42a65a76735b98e}") )
+		ERR(goto done);
+
+	entry->print(entry);
+	fputc('\n', stdout);
+
+
+	INIT(NAAAIM, ExchangeEvent, event, ERR(goto done));
+	if ( !event->parse(event, entry) )
+		ERR(goto done);
+	if ( !event->measure(event) )
+		ERR(goto done);
+	if ( !event->get_identity(event, bufr) )
+		ERR(goto done);
+	event->dump(event);
+
+	fputs("\nMeasurement:\n", stdout);
+	bufr->print(bufr);
+
+	fputs("\nEvent elements:\n", stdout);
+	entry->reset(entry);
+	if ( !event->format(event, entry) )
+		ERR(goto done);
+	entry->print(entry);
+
+	/* Re-parse based on formatted event. */
+	event->reset(event);
+	if ( !event->parse(event, entry) )
+		ERR(goto done);
+	if ( !event->measure(event) )
+		ERR(goto done);
+
+	bufr->reset(bufr);
+	if ( !event->get_identity(event, bufr) )
+		ERR(goto done);
+	fputs("\nMeasurement after re-parse:\n", stdout);
+	bufr->print(bufr);
+
+	retn = 0;
+
+
+ done:
+	WHACK(bufr);
+	WHACK(entry);
+	WHACK(event);
+
+	return retn;
+}
