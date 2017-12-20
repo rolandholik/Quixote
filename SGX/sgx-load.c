8745296084fdf66888ddcc5f6a903a166e6799be
Dr. Greg Wettstein <gw@idfusion.org>
2017-12-20 01:36:07 -0600

    Add support for loading of production enclaves.
    This update is focused on providing support for loading
    production enclave, ie. enclaves which do not have their debug
    bit sit.  This option is selected by the -p command-line
    arguement.
    
    In addition to this functionality change a usage function was
    added which outputs program header and copyright information, an
    optional error message and general usage instructions.

--- SGX/sgx-load.c
+++ SGX/sgx-load.c
@@ -1,3 +1,21 @@
+/** \file
+ * This file contains the implementation of a utility which loads
+ * and initializes an SGX enclave.  This utility is primarily useful
+ * for testing the IDfusion SGX runtime infrastructure.  It also
+ * provides a framework for generating extensive diagnostic information
+ * on the enclave loading process.
+ */
+
+/*
+ * (C)Copyright 2017, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ */
+
+/* Definitions local to this file. */
+#define PGM "sgx-load"
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -18,10 +36,48 @@
 #include "SGXenclave.h"
 
 
+/**
+ * Internal public function.
+ *
+ * This method implements outputting of an error message and status
+ * information on how to run the utility.
+ *
+ * \param err	A pointer to a null-terminated buffer holding the
+ *		error message to be output.
+ *
+ * \return	No return value is defined.
+ */
+
+static void usage(char *err)
+
+{
+	fprintf(stdout, "%s: SGX enclave loader.\n", PGM);
+	fprintf(stdout, "%s: (C)IDfusion, LLC\n", PGM);
+
+	if ( err != NULL )
+		fprintf(stdout, "\n%s", err);
+
+	fputc('\n', stdout);
+	fputs("Usage:\n", stdout);
+	fputs("\t-d:\tEnable debug mode.\n", stdout);
+	fputs("\t-p:\tGenerate token for a non-debug enclave.\n\n", stdout);
+
+	fputs("\t-e:\tEnclave to generate token for.\n", stdout);
+	fputs("\t-n:\tSGX device node.\n\t\t\tdefault = /dev/isgx\n", stdout);
+	fputs("\t-t:\tThe file containing the initialization token\n\n", \
+	      stdout);
+
+	return;
+}
+
+
+/* Main program starts here. */
+
 extern int main(int argc, char *argv[])
 
 {
-	_Bool debug = true;
+	_Bool debug	    = false,
+	      debug_enclave = true;
 
 	char *token	   = NULL,
 	     *sgx_device   = "/dev/isgx",
@@ -40,11 +96,15 @@ extern int main(int argc, char *argv[])
 
 
 	/* Parse and verify arguements. */
-	while ( (opt = getopt(argc, argv, "de:t:")) != EOF )
+	while ( (opt = getopt(argc, argv, "dpe:n:t:")) != EOF )
 		switch ( opt ) {
 			case 'd':
 				debug = true;
 				break;
+			case 'p':
+				debug_enclave = false;
+				break;
+
 			case 'e':
 				enclave_name = optarg;
 				break;
@@ -57,19 +117,22 @@ extern int main(int argc, char *argv[])
 		}
 
 	if ( enclave_name == NULL ) {
-		fputs("No enclave name specifed.\n", stderr);
+		usage("No enclave name specifed.\n");
 		goto done;
 	}
 
 
 	/* Load the launch token. */
 	if ( token == NULL ) {
-		fputs("No token specified.\n", stderr);
+		usage("No token specified.\n");
 		goto done;
 	}
 
 	INIT(HurdLib, Buffer, bufr, ERR(goto done));
 	INIT(HurdLib, File, token_file, ERR(goto done));
+
+	if ( debug )
+		fprintf(stdout, "Loading enclave token: %s\n\n", token);
 
 	token_file->open_ro(token_file, token);
 	if ( !token_file->slurp(token_file, bufr) )
@@ -80,7 +143,13 @@ extern int main(int argc, char *argv[])
 	/* Load and initialize the enclave. */
 	INIT(NAAAIM, SGXenclave, enclave, ERR(goto done));
 
-	if ( !enclave->open_enclave(enclave, sgx_device, enclave_name, debug) )
+	if ( debug ) {
+		fprintf(stdout, "Loading enclave: %s\n\n", enclave_name);
+		enclave->debug(enclave, true);
+	}
+
+	if ( !enclave->open_enclave(enclave, sgx_device, enclave_name, \
+				    debug_enclave) )
 		ERR(goto done);
 
 	if ( !enclave->create_enclave(enclave) )
