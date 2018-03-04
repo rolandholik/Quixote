889ff3cd8081c3a9761e2961082807ecbd7848a1
Dr. Greg Wettstein <gw@idfusion.org>
2018-03-04 13:12:14 -0600

    Add the OpenSSL ocall file.
    Another error was noted in the implementation of the SGX/OpenSSL
    support.  While a reference to the ssl-ocall.c file had been
    added to the Makefile the file itself had not been added to the
    GIT repository.
    
    So much for bisectability.
    
    This update adds the needed file.

---
+++ SGX/SGXlibs/SGXnaaaim/ssl-ocall.c
@@ -0,0 +1,62 @@
+#include <sgx_edger8r.h>
+#include <sgx_trts.h>
+
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define CHECK_REF_POINTER(ptr, siz) do {	\
+	if (!(ptr) || ! sgx_is_outside_enclave((ptr), (siz)))	\
+		return SGX_ERROR_INVALID_PARAMETER;\
+} while (0)
+
+#define CHECK_UNIQUE_POINTER(ptr, siz) do {	\
+	if ((ptr) && ! sgx_is_outside_enclave((ptr), (siz)))	\
+		return SGX_ERROR_INVALID_PARAMETER;\
+} while (0)
+
+typedef struct ms_sgx_oc_cpuidex_t {
+	int* ms_cpuinfo;
+	int ms_leaf;
+	int ms_subleaf;
+} ms_sgx_oc_cpuidex_t;
+
+sgx_status_t SGX_CDECL sgx_oc_cpuidex(int cpuinfo[4], int leaf, int subleaf)
+{
+	sgx_status_t status = SGX_SUCCESS;
+	size_t _len_cpuinfo = 4 * sizeof(*cpuinfo);
+
+	ms_sgx_oc_cpuidex_t* ms = NULL;
+	size_t ocalloc_size = sizeof(ms_sgx_oc_cpuidex_t);
+	void *__tmp = NULL;
+
+	ocalloc_size += (cpuinfo != NULL && sgx_is_within_enclave(cpuinfo, _len_cpuinfo)) ? _len_cpuinfo : 0;
+
+	__tmp = sgx_ocalloc(ocalloc_size);
+	if (__tmp == NULL) {
+		sgx_ocfree();
+		return SGX_ERROR_UNEXPECTED;
+	}
+	ms = (ms_sgx_oc_cpuidex_t*)__tmp;
+	__tmp = (void *)((size_t)__tmp + sizeof(ms_sgx_oc_cpuidex_t));
+
+	if (cpuinfo != NULL && sgx_is_within_enclave(cpuinfo, _len_cpuinfo)) {
+		ms->ms_cpuinfo = (int*)__tmp;
+		__tmp = (void *)((size_t)__tmp + _len_cpuinfo);
+		memcpy(ms->ms_cpuinfo, cpuinfo, _len_cpuinfo);
+	} else if (cpuinfo == NULL) {
+		ms->ms_cpuinfo = NULL;
+	} else {
+		sgx_ocfree();
+		return SGX_ERROR_INVALID_PARAMETER;
+	}
+
+	ms->ms_leaf = leaf;
+	ms->ms_subleaf = subleaf;
+	status = sgx_ocall(1, ms);
+
+	if (cpuinfo) memcpy((void*)cpuinfo, ms->ms_cpuinfo, _len_cpuinfo);
+
+	sgx_ocfree();
+	return status;
+}
