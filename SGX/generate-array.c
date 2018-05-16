34826d94df1f97622b78c159f10bc1a5fc433a71
Dr. Greg Wettstein <gw@idfusion.org>
2018-05-15 20:35:31 -0500

    Implement dynamic generation of binary arrays.
    This commit implements support for generating arrays which are
    used to encode the following binary information:
    
    	Launch enclave signature structure.
    	Launch enclave whitelist.
    
    Both of these binary structures have previously been embedded in
    appropriate C source files.  The array definitions were generated
    by a utility formerly named gen-white-list.  This utility has now
    been renamed to generate-array to indicate its more generic use.
    
    The utility was modified to take as command-line arguements the
    name of a file that is to be binary encoded and the name of the
    array that is to be generated.  Support for usage information was
    also implemented in the utility.
    
    The Makefile was modified to dynamically build the sigstruct.h
    and the LE_whitelist.h include files as a pre-requisite for the C
    source files that require them.  These files are automatically
    removed by the clean: target.
    
    The dynamic generation assumes that the signature structure is in
    a location consistent with where the Intel PSW installs the file.
    The white list is assumed to be previsouly downloaded and in a
    file in the build directory named sgx_white_list_cert.bin.  This
    is the default name of the file provided from the Intel SGX
    services site.
    
    This whitelist file can be downloaded with the following command:
    
    wget http://whitelist.trustedservices.intel.com/SGX/LCWL/Linux/sgx_white_list_cert.bin
    
    Arguably this process should be automated as part of the build
    process but at the current time, due to a desire to precisely
    specify the whitelist, the strategy will be to assume the file
    has been previously downloaded by the builder.

---
+++ SGX/generate-array.c
@@ -0,0 +1,150 @@
+/** \file
+ * This file contains the implementation of a utility which reads
+ * a binary file and generates to standard output an encoding of
+ * the file in the form of an unsigned character array.
+ */
+
+/*
+ * (C)Copyright 2018, IDfusion, LLC. All rights reserved.
+ *
+ * Please refer to the file named COPYING in the top of the source tree
+ * for licensing information.
+ */
+
+/* Definitions local to this file. */
+#define PGM "generate-array"
+#define COPYRIGHT "2018"
+
+
+/* Include files. */
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <Origin.h>
+#include <HurdLib.h>
+#include <Buffer.h>
+#include <String.h>
+#include <File.h>
+
+#include "NAAAIM.h"
+
+
+/**
+ * Internal public function.
+ *
+ * This method implements outputting of an error message and status
+ * information on how to run the utility.
+ *
+ * \param err	A pointer to a null-terminated buffer holding the
+ *		error message to be output.
+ *
+ * \return	No return value is defined.
+ */
+
+static void usage(char *err)
+
+{
+	fprintf(stdout, "%s: Binary array generation utility.\n", PGM);
+	fprintf(stdout, "%s: (C)%s IDfusion, LLC\n", PGM, COPYRIGHT);
+
+	if ( err != NULL )
+		fprintf(stdout, "\n%s", err);
+
+	fputc('\n', stdout);
+	fputs("\nArguments:\n", stdout);
+	fputs("\t-i:\tName of file containing the array contents.\n", stdout);
+	fputs("\t-n:\tThe name of the array to be generated.\n", stdout);
+
+	return;
+}
+
+
+/*
+ * Main program.
+ */
+
+extern int main(int argc, char *argv[])
+
+{
+	_Bool retn = false;
+
+	char *infile	 = NULL,
+	     *array_name = NULL;
+
+	uint8_t *sp;
+
+	int opt;
+
+	size_t lp;
+
+	Buffer bufr = NULL;
+
+	File file = NULL;
+
+
+	/* Parse and verify arguements. */
+	while ( (opt = getopt(argc, argv, "i:n:")) != EOF )
+		switch ( opt ) {
+			case 'i':
+				infile = optarg;
+				break;
+			case 'n':
+				array_name = optarg;
+				break;
+		}
+
+
+	if ( infile == NULL ) {
+		usage("No input file name specified.");
+		goto done;
+	}
+
+	if ( array_name == NULL ) {
+		usage("No array name specified.");
+		goto done;
+	}
+
+
+	/* Slurp in the input file. */
+	INIT(HurdLib, Buffer, bufr, ERR(goto done));
+	INIT(HurdLib, File, file, ERR(goto done));
+
+	file->open_ro(file, infile);
+	if ( !file->slurp(file, bufr) ) {
+		fprintf(stderr, "%s: Error reading input file.\n", PGM);
+		goto done;
+	}
+
+
+	/* Generate the named array of the given size. */
+	fprintf(stdout, "static uint8_t %s[%lu] = {\n\t", array_name, \
+		bufr->size(bufr));
+
+	sp = (uint8_t *) bufr->get(bufr);
+	for (lp= 1; lp <= bufr->size(bufr); ++lp) {
+		fprintf(stdout, "0x%02x", sp[lp-1]);
+		if ( (lp % 8) == 0 ) {
+			if ( lp == bufr->size(bufr) )
+				fputs("  \\\n", stdout);
+			else
+				fputs(", \\\n\t", stdout);
+		}
+		else
+			fputs(", ", stdout);
+	}
+	fputs("};\n", stdout);
+
+	retn = true;
+
+
+ done:
+	WHACK(bufr);
+	WHACK(file);
+
+	return retn ? 0 : 1;
+}
