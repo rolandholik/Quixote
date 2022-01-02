0c102119a3ddc1cf2c1a46856fb50ea9bd78aa59
Dr. Greg <greg@enjellic.com>
2022-01-02 10:18:22 -0600

    Implement initial USB TTY communications.
    This commit provides an initial implementation of stable
    communications between the host and the Nordic SanchoMCU
    implementation.  The Nordic SanchoMCU TTYduct implementation is
    based on a native USB ACM-CDC implementation, unlike the STmicro
    implementation that was implemented via a ACM-CDC communications
    channel supplied by the ST-link debugger implementation.
    
    The latter implementation was used on the STmicro SanchoMCU
    implementation secondary to what appears to be issues with the
    USB implementation in the STmicro development kit.
    
    The initial framework for the NAAAIM support library was
    implemented but only for the purpose of defining the TTYduct
    objec timplemenation.  The next commit will provide the NAAAIM
    support that will be needed to implement the modeling engine.
    
    The USB communications infrastructure was migrated from the
    sancho.c file to the TTYduct object since there is no need to
    expose this functionality beyond the TTYduct object.
    
    A significant factor in getting a functional implementation was
    to understand the Nordic SDK implementation and the fact that the
    app_usbd_event_queue_process() function needs to be called in
    order to process the USB event queue.  Without this the event
    handlers are not invoked and no USB communication events are
    processed.
    
    The TTYduct object needs to be migrated to the actual NAAAIM
    library directory but this will be done in combination with a
    similar code movement for the STmicro implementation.

---
+++ Sancho/Nordic/NRF52840-DK/Sancho/TTYduct.c
@@ -0,0 +1,707 @@
+/** \file
+ * This file provides the implementation of an object that provides
+ * packet based communications over a tty device using interrupt
+ * based input.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2021, Enjellic Systems Development, LLC. All rights reserved.
+ **************************************************************************/
+
+/* Include files. */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#include <nrf_log.h>
+#include <nrf_log_ctrl.h>
+#include <nrf_log_default_backends.h>
+
+#include <nrf_drv_usbd.h>
+#include <app_usbd_core.h>
+#include <app_usbd.h>
+#include <app_usbd_string_desc.h>
+#include <app_usbd_serial_num.h>
+#include <app_usbd_cdc_acm.h>
+
+#include <HurdLib.h>
+
+#include <Origin.h>
+#include <Buffer.h>
+#include <String.h>
+
+#include <NAAAIM.h>
+#include "TTYduct.h"
+
+/* State extraction macro. */
+#define STATE(var) CO(TTYduct_State, var) = this->state
+
+/* Maximum receive buffer size - 1K. */
+#define MAX_RECEIVE_SIZE 1024
+
+/* Verify library/object header file inclusions. */
+#if !defined(NAAAIM_LIBID)
+#error Library identifier not defined.
+#endif
+
+#if !defined(NAAAIM_TTYduct_OBJID)
+#error Object identifier not defined.
+#endif
+
+
+/** LocalDuct private state information. */
+struct NAAAIM_TTYduct_State
+{
+	/* The root object. */
+	Origin root;
+
+	/* Library identifier. */
+	uint32_t libid;
+
+	/* Object identifier. */
+	uint32_t objid;
+
+	/* Object status. */
+	_Bool poisoned;
+
+	/* End of transmission flag. */
+	_Bool eof;
+
+	/* Debug flag. */
+	_Bool debug;
+	/* Error code. */
+	int error;
+
+	/* Output file descriptor. */
+	int fd;
+};
+
+
+/** Flag variable indicating the USB port has been opened. */
+static _Bool Port_Open = false;
+
+/** Flag variable indicating that a read has completed. */
+static _Bool Have_Read = false;
+
+/** Flag variable indicating that a transmit is complete. */
+static _Bool TX_Done = false;
+
+/* Amount of data to be received. */
+static uint32_t Receive_Size;
+
+/* Amount of receiver buffer occupied. */
+static uint32_t Input_Size;
+static uint8_t Input_Byte;
+static uint8_t Input_Buffer[MAX_RECEIVE_SIZE];
+
+/* Input state. */
+static enum {
+	receiving_sync=0,
+	receiving_size,
+	receiving_buffer,
+	received_buffer
+} Receive_State = receiving_sync;
+
+
+/* Forward declaration for the USB event handlers. */
+static void acm_event_handler(app_usbd_class_inst_t const * p_inst, \
+			      app_usbd_cdc_acm_user_event_t event);
+
+static void usb_event_handler(app_usbd_event_type_t event);
+
+
+
+APP_USBD_CDC_ACM_GLOBAL_DEF(USB_cdc_acm,			\
+			    acm_event_handler,			\
+			    0,					\
+			    1,					\
+			    NRF_DRV_USBD_EPIN2,			\
+			    NRF_DRV_USBD_EPIN1,			\
+			    NRF_DRV_USBD_EPOUT2,		\
+			    APP_USBD_CDC_COMM_PROTOCOL_AT_V250);
+
+/** USB CDC/ACM class definition. */
+static app_usbd_config_t USB_config = {
+	.ev_state_proc = usb_event_handler
+};
+
+static app_usbd_class_inst_t const * USB_cdc_acm_class;
+
+static void _acm_event_read(app_usbd_cdc_acm_t const * acm)
+
+{
+#if 0
+	ret_code_t rc;
+
+	size_t available,
+	       read_size;
+
+	read_size = app_usbd_cdc_acm_rx_size(acm);
+	available = app_usbd_cdc_acm_bytes_stored(acm);
+	NRF_LOG_INFO("%s: Called, read=%lu, available=%lu", __func__, \
+		     read_size, available);
+#endif
+
+	app_usbd_cdc_acm_read(&USB_cdc_acm, &Input_Byte, \
+			      sizeof(Input_Byte));
+#if 0
+	NRF_LOG_INFO("%s: rc=%d, Input=%c/%02x", __func__, rc, Input_Byte, \
+		     Input_Byte);
+#endif
+
+	return;
+}
+
+
+static void acm_event_handler(app_usbd_class_inst_t const * p_inst, \
+			      app_usbd_cdc_acm_user_event_t event)
+
+{
+	app_usbd_cdc_acm_t const * p_cdc_acm = \
+		app_usbd_cdc_acm_class_get(p_inst);
+
+
+	switch ( event ) {
+		case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
+#if 1
+			NRF_LOG_INFO("PORT OPEN");
+#endif
+			Port_Open = true;
+			_acm_event_read(p_cdc_acm);
+			break;
+
+		case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
+#if 1
+			NRF_LOG_INFO("RX_DONE");
+#endif
+			Have_Read = true;
+			_acm_event_read(p_cdc_acm);
+			break;
+
+		case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+			TX_Done = true;
+#if 1
+			NRF_LOG_INFO("TX_DONE");
+#endif
+			break;
+
+		case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+#if 1
+			NRF_LOG_INFO("PORT CLOSE");
+#endif
+			break;
+	}
+
+	return;
+}
+			
+
+static void usb_event_handler(app_usbd_event_type_t event)
+
+{
+	switch ( event ) {
+		case APP_USBD_EVT_DRV_SUSPEND:
+			break;
+
+		case APP_USBD_EVT_DRV_RESUME:
+			break;
+
+		case APP_USBD_EVT_STARTED:
+			break;
+
+		case APP_USBD_EVT_STOPPED:
+			app_usbd_disable();
+			break;
+
+		case APP_USBD_EVT_POWER_DETECTED:
+			if ( !nrf_drv_usbd_is_enabled() )
+				app_usbd_enable();
+			break;
+
+		case APP_USBD_EVT_POWER_REMOVED:
+			app_usbd_stop();
+			break;
+
+		case APP_USBD_EVT_POWER_READY:
+			app_usbd_start();
+			break;
+
+		case APP_USBD_EVT_DRV_RESET:
+			break;
+		case APP_USBD_EVT_DRV_WUREQ:
+			break;
+		case APP_USBD_EVT_DRV_SETUP:
+			break;
+		case APP_USBD_EVT_DRV_EPTRANSFER:
+			break;
+		case APP_USBD_EVT_FIRST_POWER:
+			break;
+		case APP_USBD_EVT_FIRST_APP:
+			break;
+		case APP_USBD_EVT_INST_REMOVE:
+			break;
+		case APP_USBD_EVT_STATE_CHANGED:
+			break;
+		case APP_USBD_EVT_FIRST_INTERNAL:
+			break;
+		case APP_USBD_EVT_START_REQ:
+			break;
+		case APP_USBD_EVT_STOP_REQ:
+			break;
+		case APP_USBD_EVT_SUSPEND_REQ:
+			break;
+		case APP_USBD_EVT_WAKEUP_REQ:
+			break;
+		case APP_USBD_EVT_SETUP_SETADDRESS:
+			break;
+		case APP_USBD_EVT_DRV_SOF:
+			break;
+	}
+
+
+	return;
+}
+
+
+/* Replacements for byte swapping functions. */
+static inline uint32_t htonl(uint32_t value)
+
+{
+	return value >> 24 | (value >> 8 & 0xff00) | \
+		(value << 8 & 0xff0000) | value << 24;
+}
+
+static inline uint32_t ntohl(uint32_t value)
+
+{
+	return value >> 24 | (value >> 8 & 0xff00) | \
+		(value << 8 & 0xff0000) | value << 24;
+}
+
+
+/**
+ * Internal static function.
+ *
+ * This function manages the USB incoming state.
+ *
+ * \return	No return value is defined.
+ */
+
+static void receive_handler(void)
+
+{
+	static uint8_t sync_char = '@';
+
+	switch ( Receive_State ) {
+		case receiving_sync:
+			if ( Input_Byte == '\n' ) {
+				app_usbd_cdc_acm_write(&USB_cdc_acm, \
+						       &sync_char,   \
+						       sizeof(sync_char));
+				Receive_State = receiving_size;
+				goto done;
+			}
+			break;
+
+		case receiving_size:
+		        Input_Buffer[Input_Size] = Input_Byte;
+			if ( ++Input_Size < sizeof(uint32_t) )
+				goto done;
+
+			memcpy(&Receive_Size, Input_Buffer, \
+			       sizeof(Receive_Size));
+			Receive_Size  = ntohl(Receive_Size);
+			Receive_State = receiving_buffer;
+
+			memset(Input_Buffer, '\0', Input_Size);
+			Input_Size = 0;
+			break;
+
+		case receiving_buffer:
+		        Input_Buffer[Input_Size] = Input_Byte;
+			if ( ++Input_Size == Receive_Size )
+				Receive_State = received_buffer;
+			break;
+
+		case received_buffer:
+			break;
+	}
+
+
+ done:
+	return;
+}
+
+
+/**
+ * Internal private method.
+ *
+ * This method is responsible for initializing the NAAAIM_LocalDuct_State
+ * structure which holds state information for each instantiated object.
+ * The object is started out in poisoned state to catch any attempt
+ * to use the object without initializing it.
+ *
+ * \param S A pointer to the object containing the state information which
+ *        is to be initialized.
+ */
+
+static void _init_state(CO(TTYduct_State, S)) {
+
+	S->libid = NAAAIM_LIBID;
+	S->objid = NAAAIM_TTYduct_OBJID;
+
+
+	S->poisoned = false;
+	S->debug    = false;
+	S->fd	    = fileno(stdout);
+
+	return;
+}
+
+
+/**
+ * External public method.
+ *
+ * This method initializes a local device for communications.
+ *
+ * \param this	The communications object for which the device is to be
+ *		opened.
+ *
+ * \param path	The path to the device to be used for this object.
+ *
+ * \return	If the tty device is successfully opened and initialized
+ *		a boolean true value is returned.  If open or
+ *		initialization fails a false value is returned and the
+ *		object is poisoned.
+ */
+
+static _Bool init_device(CO(TTYduct, this), CO(char *, path))
+
+{
+	bool retn = false;
+
+
+	app_usbd_serial_num_generate();
+	app_usbd_init(&USB_config);
+
+	app_usbd_cdc_acm_class_inst_get(&USB_cdc_acm);
+	USB_cdc_acm_class = app_usbd_cdc_acm_class_inst_get(&USB_cdc_acm);
+	app_usbd_class_append(USB_cdc_acm_class);
+
+	app_usbd_enable();
+	app_usbd_start();
+
+	/* Clear the input buffer. */
+	Input_Size    = 0;
+	memset(Input_Buffer, '\0', sizeof(Input_Buffer));
+
+	Receive_Size  = 0;
+	Receive_State = receiving_sync;
+
+	retn = true;
+	return retn;
+}
+
+
+/**
+ * External public method.
+ *
+ * This method implements checking for the acceptance of a connection
+ * on a USB port.
+ *
+ * \param this	The communications object which connection acceptance
+ *		is to be checked.
+ *
+ * \return	A boolean value is used to indicate whether or not
+ *		a connection has been detected.  A false value
+ *		indicates there is no connection while a true value
+ *		indicates a connection is available.
+ */
+
+static _Bool accept_connection(CO(TTYduct, this))
+
+{
+	if ( Receive_State == receiving_sync ) {
+		while ( Receive_State == receiving_sync ) {
+			if ( app_usbd_event_queue_process() ) {
+				if ( Have_Read ) {
+					receive_handler();
+					Have_Read = false;
+				}
+			}
+			__WFE();
+		}
+	}
+
+
+	return Port_Open;
+}
+
+
+/**
+ * External public method.
+ *
+ * This method implements sending the contents of a specified Buffer object
+ * over the connection represented by the callingn object.
+ *
+ * \param this	The LocalDuct object over which the Buffer is to be sent.
+ *
+ * \return	A boolean value is used to indicate whether or the
+ *		write was successful.  A true value indicates the
+ *		transmission was successful.
+ */
+
+static _Bool send_Buffer(CO(TTYduct, this), CO(Buffer, bf))
+
+{
+	STATE(S);
+
+	uint8_t *sp;
+
+#if 0
+	uint8_t tx_bufr[NRF_DRV_USBD_EPSIZE];
+#endif
+
+	uint32_t blocks,
+		 residual;
+
+	_Bool retn = false;
+
+	uint32_t size = htonl(bf->size(bf));
+
+	Buffer bufr = NULL;
+
+
+	if ( S->poisoned )
+		ERR(goto done);
+	if ( (bf == NULL) || bf->poisoned(bf))
+		ERR(goto done);
+
+
+	INIT(HurdLib, Buffer, bufr, ERR(goto done));
+	bufr->add(bufr, (unsigned char *) &size, sizeof(size));
+	if ( !bufr->add_Buffer(bufr, bf) )
+		ERR(goto done);
+
+	if ( S->debug ) {
+		fputs("Sending buffer.\n", stdout);
+		bufr->hprint(bufr);
+		fflush(stdout);
+	}
+
+
+	/* Calculate blocks and residuals based on USB packet size. */
+	blocks	 = bufr->size(bufr) / NRF_DRV_USBD_EPSIZE;
+	residual = bufr->size(bufr) % NRF_DRV_USBD_EPSIZE;
+#if 1
+	NRF_LOG_INFO("%s: sending blocks=%u, residual=%u", __func__, blocks, \
+		     residual);
+#endif
+
+
+	/* Send blocks and residual. */
+	sp = bufr->get(bufr);
+
+	while ( blocks-- ) {
+		TX_Done = false;
+		app_usbd_cdc_acm_write(&USB_cdc_acm, sp, NRF_DRV_USBD_EPSIZE);
+		sp += NRF_DRV_USBD_EPSIZE;
+
+		while ( !TX_Done ) {
+			app_usbd_event_queue_process();
+		}
+	}
+
+	if ( residual != 0 ) {
+		TX_Done = false;
+		app_usbd_cdc_acm_write(&USB_cdc_acm, sp, residual);
+
+		while ( !TX_Done ) {
+			app_usbd_event_queue_process();
+		}
+	}
+
+	retn = true;
+
+
+ done:
+	if ( !retn )
+		S->poisoned = true;
+
+	WHACK(bufr);
+
+	return retn;
+}
+
+
+/**
+ * External public method.
+ *
+ * This method implements loading the specified number of bytes into
+ * the provided Buffer object.
+ *
+ * \param this	The object that data is to be read into.
+ *
+ * \return	A boolean value is used to indicate whether or the
+ *		read was successful.  A true value indicates the receive
+ *		was successful.
+ */
+
+static _Bool receive_Buffer(CO(TTYduct, this), CO(Buffer, bf))
+
+{
+	STATE(S);
+
+	_Bool retn = false;
+
+
+	/* Validate object. */
+	if ( S->poisoned )
+		ERR(goto done);
+	if ( (bf == NULL) || bf->poisoned(bf) )
+		ERR(goto done);
+
+
+	NRF_LOG_INFO("Waiting for size.");
+
+	/* Block until USB receive handler has a size. */
+	while ( Receive_State == receiving_size ) {
+		if ( app_usbd_event_queue_process() ) {
+			if ( Have_Read ) {
+				receive_handler();
+				Have_Read = false;
+			}
+		}
+		__WFE();
+	}
+	NRF_LOG_INFO("Have size: %lu", Receive_Size);
+
+
+	/* Block until the USB receive handler has the packet being sent. */
+	while ( Receive_State == receiving_buffer ) {
+		if ( app_usbd_event_queue_process() ) {
+			if ( Have_Read ) {
+				receive_handler();
+				Have_Read = false;
+			}
+		}
+		__WFE();
+	}
+	NRF_LOG_INFO("Have buffer.");
+	
+
+
+	/* Load the buffer with the input stream and reset input. */
+	if ( !bf->add(bf, Input_Buffer, Receive_Size) )
+		ERR(goto done);
+	retn = true;
+
+
+ done:
+	Input_Size = 0;
+	memset(Input_Buffer, '\0', sizeof(Input_Buffer));
+
+	Receive_Size  = 0;
+	Receive_State = receiving_size;
+
+	if ( !retn )
+		S->poisoned = true;
+
+	return retn;
+}
+
+
+/**
+ * External public method.
+ *
+ * This method activates debug mode for the object.
+ *
+ * \param this	A pointer to the object which is to have debug mode
+ *		activated.
+ */
+
+static void debug(CO(TTYduct, this))
+
+{
+	STATE(S);
+
+
+	S->debug = true;
+
+	return;
+}
+
+
+/**
+ * External public method.
+ *
+ * This method implements a destructor for a LocalDuct object.
+ *
+ * \param this	A pointer to the object which is to be destroyed.
+ */
+
+static void whack(CO(TTYduct, this))
+
+{
+	STATE(S);
+
+
+	/* Destroy resources. */
+	S->root->whack(S->root, this, S);
+
+	return;
+}
+
+
+/**
+ * External constructor call.
+ *
+ * This function implements a constructor call for a LocalDuct object.
+ *
+ * \return	A pointer to the initialized LocalDuct.  A null value
+ *		indicates an error was encountered in object generation.
+ */
+
+extern TTYduct NAAAIM_TTYduct_Init(void)
+
+{
+	Origin root;
+
+	TTYduct this = NULL;
+
+	struct HurdLib_Origin_Retn retn;
+
+
+	/* Get the root object. */
+	root = HurdLib_Origin_Init();
+
+	/* Allocate the object and internal state. */
+	retn.object_size  = sizeof(struct NAAAIM_TTYduct);
+	retn.state_size   = sizeof(struct NAAAIM_TTYduct_State);
+	if ( !root->init(root, NAAAIM_LIBID, NAAAIM_TTYduct_OBJID, &retn) )
+		return NULL;
+	this	    	  = retn.object;
+	this->state 	  = retn.state;
+	this->state->root = root;
+
+	/* Initialize aggregate objects. */
+
+	/* Initialize object state. */
+	_init_state(this->state);
+
+	/* Method initialization. */
+	this->init_device	= init_device;
+	this->accept_connection	= accept_connection;
+
+	this->send_Buffer	= send_Buffer;
+	this->receive_Buffer	= receive_Buffer;
+
+	this->debug		= debug;
+
+	this->whack		= whack;
+
+	return this;
+}
--- Sancho/Nordic/NRF52840-DK/Sancho/sancho.c
+++ Sancho/Nordic/NRF52840-DK/Sancho/sancho.c
@@ -22,180 +22,27 @@
 #include <bsp.h>
 #include <bsp_cli.h>
 
-#include <nrf_drv_usbd.h>
-#include <app_usbd_core.h>
-#include <app_usbd.h>
-#include <app_usbd_string_desc.h>
-#include <app_usbd_serial_num.h>
-#include <app_usbd_cdc_acm.h>
-
 #include <nrf_cli_uart.h>
 #include <nrf_log.h>
 #include <nrf_log_ctrl.h>
 #include <nrf_log_default_backends.h>
 
+#include <HurdLib.h>
+#include <Buffer.h>
 
-/* Function declarations. */
-static void acm_event_handler(app_usbd_class_inst_t const * p_inst, \
-			      app_usbd_cdc_acm_user_event_t event);
+#include <NAAAIM.h>
+#include <TTYduct.h>
 
 
 /* Static variable definitions. */
 
-/** Flag to indicate that the USB port has been opened. */
-static _Bool Port_Open = false;
-
-/** Flag to indicate that I/O is available from read. */
-static _Bool Have_Read = false;
-
-/** USB read buffer. */
-static char RXbuffer[1];
-
-/** USB transmit buffer. */
-static char TXbuffer[NRF_DRV_USBD_EPSIZE];
-
-
-/** UART definition. */
+/** UART definition for sancho console. */
 NRF_CLI_UART_DEF(sancho_console_uart, 0, 64, 16);
 NRF_CLI_DEF(sancho_console,
             "sancho_console:~$ ",
             &sancho_console_uart.transport,
             '\r',
             4);
-
-
-APP_USBD_CDC_ACM_GLOBAL_DEF(cmd_cdc_acm,			\
-			    acm_event_handler,			\
-			    0,					\
-			    1,					\
-			    NRF_DRV_USBD_EPIN2,			\
-			    NRF_DRV_USBD_EPIN1,			\
-			    NRF_DRV_USBD_EPOUT2,		\
-			    APP_USBD_CDC_COMM_PROTOCOL_AT_V250);
-
-
-static void _acm_event_read(app_usbd_cdc_acm_t const * acm)
-
-{
-	ret_code_t rc;
-
-	size_t read_size;
-
-
-	do {
-		read_size = app_usbd_cdc_acm_rx_size(acm);
-		rc = app_usbd_cdc_acm_read(&cmd_cdc_acm, RXbuffer, \
-					   sizeof(RXbuffer));
-		NRF_LOG_INFO("%s: Size=%lu, buffer=%c/%02x", __func__, \
-			     read_size, RXbuffer[0], RXbuffer[0]);
-
-	} while ( rc == NRF_SUCCESS );
-
-
-	return;
-}
-
-
-static void acm_event_handler(app_usbd_class_inst_t const * p_inst, \
-			      app_usbd_cdc_acm_user_event_t event)
-
-{
-	app_usbd_cdc_acm_t const * p_cdc_acm = \
-		app_usbd_cdc_acm_class_get(p_inst);
-
-
-	switch ( event ) {
-		case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
-			NRF_LOG_INFO("PORT OPEN");
-			Port_Open = true;
-			app_usbd_cdc_acm_read(&cmd_cdc_acm, RXbuffer, \
-					      sizeof(RXbuffer));
-			break;
-
-		case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
-			NRF_LOG_INFO("RX_DONE");
-			_acm_event_read(p_cdc_acm);
-			Have_Read = true;
-			break;
-
-		case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
-			NRF_LOG_INFO("TX_DONE");
-			break;
-
-		case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
-			NRF_LOG_INFO("PORT CLOSE");
-			break;
-	}
-
-	return;
-}
-			
-
-static void usb_event_handler(app_usbd_event_type_t event)
-
-{
-	switch ( event ) {
-		case APP_USBD_EVT_DRV_SUSPEND:
-			break;
-
-		case APP_USBD_EVT_DRV_RESUME:
-			break;
-
-		case APP_USBD_EVT_STARTED:
-			break;
-
-		case APP_USBD_EVT_STOPPED:
-			app_usbd_disable();
-			break;
-
-		case APP_USBD_EVT_POWER_DETECTED:
-			if ( !nrf_drv_usbd_is_enabled() )
-				app_usbd_enable();
-			break;
-
-		case APP_USBD_EVT_POWER_REMOVED:
-			app_usbd_stop();
-			break;
-
-		case APP_USBD_EVT_POWER_READY:
-			app_usbd_start();
-			break;
-
-		case APP_USBD_EVT_DRV_RESET:
-			break;
-		case APP_USBD_EVT_DRV_WUREQ:
-			break;
-		case APP_USBD_EVT_DRV_SETUP:
-			break;
-		case APP_USBD_EVT_DRV_EPTRANSFER:
-			break;
-		case APP_USBD_EVT_FIRST_POWER:
-			break;
-		case APP_USBD_EVT_FIRST_APP:
-			break;
-		case APP_USBD_EVT_INST_REMOVE:
-			break;
-		case APP_USBD_EVT_STATE_CHANGED:
-			break;
-		case APP_USBD_EVT_FIRST_INTERNAL:
-			break;
-		case APP_USBD_EVT_START_REQ:
-			break;
-		case APP_USBD_EVT_STOP_REQ:
-			break;
-		case APP_USBD_EVT_SUSPEND_REQ:
-			break;
-		case APP_USBD_EVT_WAKEUP_REQ:
-			break;
-		case APP_USBD_EVT_SETUP_SETADDRESS:
-			break;
-		case APP_USBD_EVT_DRV_SOF:
-			break;
-	}
-
-
-	return;
-}
 
 
 static void bsp_event_callback(bsp_event_t ev)
@@ -224,19 +71,13 @@ void console_init(void)
 	return;
 }
 
-				
 
 int main(void)
 
 {
-	size_t size;
+	Buffer bufr = NULL;
 
-	static app_usbd_config_t usbd_config = {
-		.ev_state_proc = usb_event_handler
-	};
-
-	app_usbd_class_inst_t const * class_cdc_acm = \
-		app_usbd_cdc_acm_class_inst_get(&cmd_cdc_acm);
+	TTYduct duct = NULL;
 
 
 	/* Platform initialization. */
@@ -253,52 +94,42 @@ int main(void)
 	console_init();
 
 
-	/* USB initialization. */
-	NRF_LOG_INFO("Starting USB configuration.");
-	app_usbd_serial_num_generate();
-
-	app_usbd_init(&usbd_config);
-	app_usbd_class_append(class_cdc_acm);
-
-	app_usbd_enable();
-	app_usbd_start();
-
-
 	/* Wait for port open event. */
-	NRF_LOG_INFO("Waiting for connection.");
-	while ( !Port_Open ) {
-		while ( app_usbd_event_queue_process() )
-			continue;
+	INIT(NAAAIM, TTYduct, duct, ERR(goto done));
+	if ( !duct->init_device(duct, NULL) )
+		ERR(goto done);
 
+	NRF_LOG_INFO("Waiting for connection.");
+	while ( !duct->accept_connection(duct) ) {
 		nrf_cli_process(&sancho_console);
 		NRF_LOG_PROCESS();
 		__WFE();
 	}
 
 
-	NRF_LOG_INFO("Starting USB loop.");
+	NRF_LOG_INFO("Starting connection loop.");
+	INIT(HurdLib, Buffer, bufr, ERR(goto done));
+
 	while ( true ) {
-		while ( app_usbd_event_queue_process() )
-			continue;
+		if ( !duct->receive_Buffer(duct, bufr) )
+			ERR(goto done);
 
-		if ( Have_Read ) {
-			NRF_LOG_INFO("Have read indication: buffer=%02x.", \
-				     RXbuffer[0]);
+		printf("Received buffer:\n");
+		bufr->hprint(bufr);
 
-			memset(TXbuffer, '\0', sizeof(TXbuffer));
-			size = snprintf(TXbuffer, sizeof(TXbuffer), "%s", \
-					RXbuffer);
-			if ( size >= sizeof(TXbuffer) )
-				continue;
-			NRF_LOG_INFO("Sending: size=%lu, char=%c/%02x", \
-				     size, TXbuffer[0], TXbuffer[0]);
-			app_usbd_cdc_acm_write(&cmd_cdc_acm, TXbuffer, 1);
-			app_usbd_cdc_acm_serial_state_notify(&cmd_cdc_acm, \
-						     APP_USBD_CDC_ACM_SERIAL_STATE_BREAK, \
-						     false);
-			Have_Read = false;
-		}
+		if ( !duct->send_Buffer(duct, bufr) )
+			ERR(goto done);
+		bufr->reset(bufr);
 
+		nrf_cli_process(&sancho_console);
+		NRF_LOG_PROCESS();
+	}
+
+
+ done:
+	NRF_LOG_INFO("Done.");
+
+	while ( true ) {
 		nrf_cli_process(&sancho_console);
 		NRF_LOG_PROCESS();
 		__WFE();
