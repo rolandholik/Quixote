c3946fffdfcb5008f9be855d61e003f0525574fd
Dr. Greg <greg@enjellic.com>
2022-01-14 20:16:56 -0600

    Add Nordic SanchoDongle implementation.
    This commit implements a SanchoMCU implementation for the
    NRF52840-DONGLE part.  The NRF52840-DONGLE is a USB 'fob' form
    factor implementation of the NRF52840-DK development board.
    
    The SanchoDongle implementation inherits much of its
    functionality from the NRF52840-DK implementation.  A board
    specific implementation of the interpreter code is provided in
    order to take advantage of the LED's on the board that are used
    to provide an indication of the status of the host<->co-processor
    implementation.

---
+++ Sancho/Nordic/NRF52840-DONGLE/Sancho/sancho-interpreter.c
@@ -0,0 +1,710 @@
+/** \file
+ * This file contains the implementation of the Sancho interpreter.
+ * This code runs in a thread that takes TTYduct encapsulated commands
+ * from the Quixote instance and executes them.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2021, Enjellic Systems Development, LLC. All rights reserved.
+ **************************************************************************/
+
+/* Module definitions. */
+#define IDSIZE 32
+
+
+/* Include files. */
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <boards.h>
+
+#include "HurdLib.h"
+#include <Buffer.h>
+#include <String.h>
+#include <SHA256.h>
+
+#include <sancho-cmd.h>
+
+#include "SecurityEvent.h"
+#include "SecurityPoint.h"
+#include "TSEM.h"
+#include "TTYduct.h"
+#include "Duct.h"
+
+#include "sancho.h"
+
+
+/**
+ * Flag variable to indicate that cellular notification is available.
+ */
+static _Bool Cellular_Enabled = false;
+
+/* Communications object to be used by all functions. */
+TTYduct Host;
+
+/* Flag to specifier interpreter error. */
+static _Bool Have_Error;
+
+/** Flag to indicate current connection light. */
+static int Connection_LED = CONNECTION_LED;
+
+
+/**
+ * Private function.
+ *
+ * This function encapsulates the sending of a response indicating
+ * that the previously executed operation has succeeded.
+ *
+ * \param duct		The object describing the communications duct
+ *			over which the response is to be sent.
+ *
+ * \param bufr		A Buffer object that will be used to send
+ *			the message.  This object will be reset upon
+ *			return.
+ *
+ * \return
+ */
+
+static void send_ok(CO(TTYduct, duct), CO(Buffer, bufr))
+
+{
+	static char *ok = "OK";
+
+
+	bufr->reset(bufr);
+
+	if ( !bufr->add(bufr, (unsigned char *) ok, strlen(ok) + 1) )
+		return;
+	duct->send_Buffer(duct, bufr);
+
+	bufr->reset(bufr);
+
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the indication that a security event that is
+ * invoking discipline of the system has been processed.
+ *
+ * \param event		The object containing the representation of
+ *			the event.
+ *
+ * \return		No return value is defined.
+ */
+
+static void _send_event(CO(String, event))
+
+{
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the updating of the platfrom behavior model
+ * with an information exchange event.
+ *
+ * \param duct		The object used to implement communications
+ *			with the host.
+ *
+ * \param model		The model instance that is to be updated.
+ *
+ * \param bufr		A Buffer object containing the exchange event
+ *			description.
+ *
+ * \return		No return value is defined.
+ */
+
+static void add_event(CO(TTYduct, duct), CO(TSEM, model), CO(Buffer, bufr))
+
+{
+	char *p,
+	     msg[80];
+
+	_Bool updated,
+	      discipline,
+	      sealed;
+
+	int msg_size;
+
+	pid_t pid;
+
+	String update = NULL;
+
+	SecurityEvent event = NULL;
+
+
+	/* Parse event. */
+	p = (char *) bufr->get(bufr);
+	if ( (p = index(p, ' ')) == NULL )
+		ERR(goto done);
+	++p;
+
+	INIT(HurdLib, String, update, ERR(goto done));
+	if ( !update->add(update, p) )
+		ERR(goto done);
+
+	INIT(NAAAIM, SecurityEvent, event, ERR(goto done));
+	if ( !event->parse(event, update) )
+		ERR(goto done);
+	if ( !model->update(model, event, &updated, &discipline, &sealed) )
+		ERR(goto done);
+
+	/* Send response. */
+	if ( discipline ) {
+		memset(msg, '\0', sizeof(msg));
+		model->discipline_pid(model, &pid);
+		msg_size = snprintf(msg, sizeof(msg) - 1, "DISCIPLINE %d", \
+				    pid);
+		bufr->reset(bufr);
+		bufr->add(bufr, (unsigned char *) msg, msg_size + 1);
+		duct->send_Buffer(duct, bufr);
+		if ( updated )
+			Connection_LED = FAULT_LED;
+	} else {
+		memset(msg, '\0', sizeof(msg));
+		event->get_pid(event, &pid);
+		msg_size = snprintf(msg, sizeof(msg) - 1, "RELEASE %d", \
+				    pid);
+		bufr->reset(bufr);
+		bufr->add(bufr, (unsigned char *) msg, msg_size + 1);
+		duct->send_Buffer(duct, bufr);
+	}
+
+
+ done:
+	WHACK(update);
+	if ( !updated )
+		WHACK(event);
+
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the addition of a hardware platform aggregate
+ * event to the specified behavior model.
+ *
+ * \param duct		The object used to implement communications
+ *			with the host.
+ *
+ * \param model		The model instance that is to be updated.
+ *
+ * \param bufr		A Buffer object containing the aggregate value
+ *			to be added.
+ *
+ * \return		No return value is defined.
+ */
+
+static void add_aggregate(CO(TTYduct, duct), CO(TSEM, model), CO(Buffer, bufr))
+
+{
+	char *p;
+
+	Buffer aggregate = NULL;
+
+
+	p = (char *) bufr->get(bufr);
+	if ( (p = index(p, ' ')) == NULL )
+		ERR(goto done);
+
+	++p;
+	if ( strlen(p) != 64 )
+		ERR(goto done);
+
+	INIT(HurdLib, Buffer, aggregate, ERR(goto done));
+	if ( !aggregate->add_hexstring(aggregate, p) )
+		ERR(goto done);
+
+	if ( model->set_aggregate(model, aggregate) )
+		send_ok(duct, bufr);
+
+
+ done:
+	WHACK(aggregate);
+
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the addition of a security event to the
+ * specified behavior model.
+ *
+ * \Param duct		The object used to implement communications
+ *			with the host.
+ *
+ * \param model		The model instance that is to be updated.
+ *
+ * \param bufr		A Buffer object containing the security event
+ *			to be added.
+ *
+ * \return		No return value is defined.
+ */
+
+static void add_security(CO(TTYduct, duct), CO(TSEM, model), CO(Buffer, bufr))
+
+{
+	String event = NULL;
+
+
+	INIT(HurdLib, String, event, ERR(goto done));
+	event->add(event, (char *) bufr->get(bufr));
+	if ( model->add_TE_event(model, event) )
+		send_ok(duct, bufr);
+
+
+ done:
+
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the loading of entries into a security
+ * model.
+ *
+ * \Param duct		The object used to implement communications
+ *			with the Quixote instance.
+ *
+ * \param model		The model instance that is to be updated.
+ *
+ * \param bufr		A Buffer object containing the entry to be
+ *			loaded into the security model.
+ *
+ * \return		No return value is defined.
+ */
+
+static void do_load(CO(TTYduct, duct), CO(TSEM, model), CO(Buffer, bufr))
+
+{
+	char *p,
+	     *load_start;
+
+	const static char error[] = "ERROR";
+
+	String entry = NULL;
+
+
+	load_start = (char *) bufr->get(bufr);
+	if ( (p = strchr(load_start, ' ')) == NULL )
+		goto done;
+	++p;
+
+	INIT(HurdLib, String, entry, ERR(goto done));
+	if ( !entry->add(entry, p) )
+		goto done;
+
+	if ( !model->load(model, entry) )
+		goto done;
+
+	send_ok(duct, bufr);
+	return;
+
+
+ done:
+	bufr->reset(bufr);
+	if ( !bufr->add(bufr, (unsigned char *) error, sizeof(error)) )
+		return;
+	duct->send_Buffer(duct, bufr);
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the output of the current behavior identities.
+ *
+ * \param duct		The object used to implement communications
+ *			with the host.
+ *
+ * \param model		The model instance whose contours are to be
+ *			displayed.
+ *
+ * \param bufr		A Buffer object to be used for displaying
+ *			the behavioral identities.
+ *
+ * \return		No return value is defined.
+ */
+
+static void send_points(CO(TTYduct, duct), CO(TSEM, model), CO(Buffer, bufr))
+
+{
+	uint8_t *p,
+		 pi;
+
+	char point[IDSIZE * 2 + 1];
+
+	size_t lp,
+	       cnt = 0;
+
+	SecurityPoint cp = NULL;
+
+
+	/*
+	 * Compute the number of elements in the list and send it to
+	 * the client.
+	 */
+	cnt = model->points_size(model);
+
+
+	bufr->reset(bufr);
+	bufr->add(bufr, (unsigned char *) &cnt, sizeof(cnt));
+	if ( !duct->send_Buffer(duct, bufr) )
+		ERR(goto done);
+
+
+	/* Send each trajectory point. */
+	model->rewind_points(model);
+
+	for (lp= 0; lp < cnt; ++lp ) {
+		if ( !model->get_point(model, &cp) )
+			ERR(goto done);
+		if ( cp == NULL )
+			continue;
+
+		memset(point, '\0', sizeof(point));
+		p = cp->get(cp);
+		for (pi= 0; pi < IDSIZE; ++pi)
+			snprintf(&point[pi*2], 3, "%02x", *p++);
+
+		bufr->reset(bufr);
+		bufr->add(bufr, (unsigned char *) point, sizeof(point));
+		if ( !duct->send_Buffer(duct, bufr) )
+			ERR(goto done);
+	}
+
+
+ done:
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the output of the current execution
+ * trajectory of the model
+ *
+ * \param duct		The object used to implement communications
+ *			with the host.
+ *
+ * \param model		The model instance whose trajectory is to
+ *			be displayed.
+ *
+ * \param bufr		A Buffer object to be used for sending
+ *			the trajectory elements.
+ *
+ * \return		No return value is defined.
+ */
+
+static void send_trajectory(CO(TTYduct, duct), CO(TSEM, model), \
+			    CO(Buffer, bufr))
+
+{
+	size_t lp,
+	       cnt = 0;
+
+	SecurityEvent event;
+
+	String es = NULL;
+
+
+	/* Return the number of trajectory items. */
+	cnt = model->trajectory_size(model);
+
+	bufr->reset(bufr);
+	bufr->add(bufr, (unsigned char *) &cnt, sizeof(cnt));
+	if ( !duct->send_Buffer(duct, bufr) )
+		ERR(goto done);
+
+
+	/* Send each trajectory point. */
+	INIT(HurdLib, String, es, ERR(goto done));
+
+	model->rewind_event(model);
+	for (lp= 0; lp < cnt; ++lp ) {
+		if ( !model->get_event(model, &event) )
+			ERR(goto done);
+		if ( event == NULL )
+			continue;
+		if ( !event->format(event, es) )
+			ERR(goto done);
+
+		bufr->reset(bufr);
+		bufr->add(bufr, (unsigned char *) es->get(es), \
+			  es->size(es) + 1);
+		if ( !duct->send_Buffer(duct, bufr) )
+			ERR(goto done);
+		es->reset(es);
+	}
+
+
+ done:
+	WHACK(es);
+
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the output of the current forensics log
+ * for the specified model.
+ *
+ * \param duct		The object used to implement communications
+ *			with the host.
+ *
+ * \param model		The model instance whose forensics are to
+ *			be displayed.
+ *
+ * \param bufr		A Buffer object to be used for sending
+ *			the forensics elements..
+ *
+ * \return		No return value is defined.
+ */
+
+static void send_forensics(CO(TTYduct, duct), CO(TSEM, model), \
+			   CO(Buffer, bufr))
+
+{
+	size_t lp,
+	       cnt = 0;
+
+	SecurityEvent event;
+
+	String es = NULL;
+
+
+	/* Return the number of trajectory items. */
+	cnt = model->forensics_size(model);
+
+	bufr->reset(bufr);
+	bufr->add(bufr, (unsigned char *) &cnt, sizeof(cnt));
+	if ( !duct->send_Buffer(duct, bufr) )
+		ERR(goto done);
+
+
+	/* Send each trajectory point. */
+	INIT(HurdLib, String, es, ERR(goto done));
+
+	model->rewind_forensics(model);
+	for (lp= 0; lp < cnt; ++lp ) {
+		if ( !model->get_forensics(model, &event) )
+			ERR(goto done);
+		if ( event == NULL )
+			continue;
+		if ( !event->format(event, es) )
+			ERR(goto done);
+
+		bufr->reset(bufr);
+		bufr->add(bufr, (unsigned char *) es->get(es), \
+			  es->size(es) + 1);
+		if ( !duct->send_Buffer(duct, bufr) )
+			ERR(goto done);
+		es->reset(es);
+	}
+
+
+ done:
+	WHACK(es);
+
+	return;
+}
+
+
+/**
+ * Private function.
+ *
+ * This function implements the output of the current security event log
+ * for the specified model.
+ *
+ * \param duct		The object used to implement communications
+ *			with the host.
+ *
+ * \param model		The model instance whose forensics are to
+ *			be displayed.
+ *
+ * \param bufr		A Buffer object to be used for sending
+ *			the events.
+ *
+ * \return		No return value is defined.
+ */
+
+static void send_events(CO(TTYduct, duct), CO(TSEM, model), CO(Buffer, bufr))
+
+{
+	size_t lp,
+	       cnt = 0;
+
+	String event = NULL;
+
+
+	/* Get the number of elements in the security list. */
+	cnt = model->TE_events_size(model);
+
+	bufr->reset(bufr);
+	bufr->add(bufr, (unsigned char *) &cnt, sizeof(cnt));
+	if ( !duct->send_Buffer(duct, bufr) )
+		ERR(goto done);
+
+
+	/* Send each event. */
+	model->TE_rewind_event(model);
+
+	for (lp= 0; lp < cnt; ++lp) {
+		if ( !model->get_TE_event(model, &event) )
+				ERR(goto done);
+		if ( event == NULL )
+				continue;
+
+		bufr->reset(bufr);
+		bufr->add(bufr, (unsigned char *) event->get(event), \
+			  event->size(event));
+		if ( !duct->send_Buffer(duct, bufr) )
+			ERR(goto done);
+	}
+
+
+done:
+	return;
+}
+
+
+static int get_command(CO(Buffer, bufr))
+
+{
+	char *bp;
+
+	struct sancho_cmd_definition *cp;
+
+
+	/* Locate the event type. */
+	bp = (char *) bufr->get(bufr);
+
+	for (cp= Sancho_cmd_list; cp->syntax != NULL; ++cp) {
+		if ( strncmp(cp->syntax, bp, strlen(cp->syntax)) == 0 )
+			break;
+	}
+
+	return cp->command;
+}
+
+
+/**
+ * External function call.
+ *
+ * This function implements the Sancho command interpreter.
+ *
+ * \param duct		The object used to implement communications
+ *			with the host.
+ *
+ * \return	No return value is defined.
+ */
+
+extern void sancho_interpreter(const TTYduct duct)
+
+{
+	Buffer bufr = NULL;
+
+	TSEM model = NULL;
+
+
+	Host = duct;
+
+	INIT(HurdLib, Buffer, bufr, ERR(goto done));
+	INIT(NAAAIM, TSEM, model, ERR(goto done));
+
+	while ( true ) {
+		if ( !duct->receive_Buffer(duct, bufr) )
+			goto done;
+
+		bsp_board_led_off(Connection_LED);
+		bsp_board_led_on(ACTIVITY_LED);
+
+		switch ( get_command(bufr) ) {
+			case exchange_event:
+				add_event(Host, model, bufr);
+				break;
+
+			case aggregate_event:
+				add_aggregate(Host, model, bufr);
+				break;
+
+			case seal_event:
+				model->seal(model);
+				send_ok(Host, bufr);
+				break;
+
+			case TE_event:
+				add_security(duct, model, bufr);
+				break;
+
+			case sancho_load:
+				do_load(duct, model, bufr);
+				break;
+
+			case show_measurement:
+				bufr->reset(bufr);
+				model->get_measurement(model, bufr);
+				duct->send_Buffer(duct, bufr);
+				break;
+
+			case show_state:
+				bufr->reset(bufr);
+				model->get_state(model, bufr);
+				duct->send_Buffer(duct, bufr);
+				break;
+
+			case show_trajectory:
+				send_trajectory(duct, model, bufr);
+				break;
+
+			case show_forensics:
+				send_forensics(duct, model, bufr);
+				break;
+
+			case show_points:
+				send_points(duct, model, bufr);
+				break;
+
+			case show_events:
+				send_events(duct, model, bufr);
+				break;
+
+			case enable_cell:
+				send_ok(Host, bufr);
+				break;
+
+			case sancho_reset:
+				break;
+		}
+
+		bsp_board_led_off(ACTIVITY_LED);
+		bsp_board_led_on(Connection_LED);
+
+		bufr->reset(bufr);
+		Have_Error = false;
+	}
+
+
+ done:
+	Connection_LED = CONNECTION_LED;
+
+	WHACK(bufr);
+	WHACK(model);
+
+	return;
+}
---
+++ Sancho/Nordic/NRF52840-DONGLE/Sancho/sancho.c
@@ -0,0 +1,110 @@
+/** \file
+ *
+ * This file contains the setup code for implenting a SanchoMCU
+ * implementation based on the Nordic 52840 development board.
+ */
+
+/**************************************************************************
+ * Copyright (c) 2020, Enjellic Systems Development, LLC. All rights reserved.
+ **************************************************************************/
+
+/* Includes. */
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <nrf.h>
+#include <nrf_drv_clock.h>
+#include <nrf_gpio.h>
+#include <nrf_delay.h>
+
+#include <boards.h>
+#include <bsp.h>
+#include <app_timer.h>
+
+#include <HurdLib.h>
+#include <Buffer.h>
+
+#include <NAAAIM.h>
+#include <TTYduct.h>
+
+#include "sancho.h"
+
+
+/* Static variable definitions. */
+
+
+#if 0
+static void bsp_event_callback(bsp_event_t ev)
+
+{
+	return;
+}
+#endif
+
+
+int main(void)
+
+{
+	TTYduct duct = NULL;
+
+
+	/* Platform initialization. */
+	nrf_drv_clock_init();
+
+	nrf_drv_clock_lfclk_request(NULL);
+	while ( !nrf_drv_clock_lfclk_is_running() )
+		continue;
+
+	app_timer_init();
+
+	bsp_board_init(BSP_INIT_LEDS);
+
+
+	/* Wait for port open event. */
+	INIT(NAAAIM, TTYduct, duct, ERR(goto done));
+	if ( !duct->init_device(duct, NULL) )
+		ERR(goto done);
+
+
+	/* Invoke the interpreter on each connection. */
+	while ( true ) {
+		bsp_board_led_on(ACTIVE_LED);
+
+		while ( !duct->accept_connection(duct) ) {
+			__WFE();
+		}
+
+
+		/* Invoke command interpreter, return on port closure. */
+		bsp_board_led_on(CONNECTION_LED);
+		sancho_interpreter(duct);
+		bsp_board_leds_off();
+	}
+
+
+ done:
+	while ( true) {
+		bsp_board_led_on(0);
+		nrf_delay_ms(500);
+		bsp_board_led_off(0);
+
+		bsp_board_led_on(1);
+		nrf_delay_ms(500);
+		bsp_board_led_off(1);
+	}
+}
+
+
+__attribute__((weak)) int _write(int file, char *ptr, int len)
+
+{
+	return len;
+}
+
+
+void Error(const char *file, const char *function, int line)
+
+{
+	return;
+}
