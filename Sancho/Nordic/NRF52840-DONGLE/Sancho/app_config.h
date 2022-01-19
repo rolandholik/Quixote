378314500d00ef6db1a68051ff6aa696c0b25462
Dr. Greg <greg@enjellic.com>
2022-01-19 03:11:13 -0600

    Implement custom configuration file.
    The Nordic development kit requires a configuration file to
    specify the large number of parameters that modify the
    compilation of code in the development kit.  Each 'project' in
    the development kit has its own configuration file.
    
    The NRF52840-DONGLE implementation uses the configuration file
    for the usbd_cdc_acm demonstration project as it most closely
    aligns with the code needed for the Sancho implementation.  This
    configuration file is used to provide definitions for the
    NRF52840-DK implementation so customizations are required.
    
    The standard Nordic configuration files have the ability to
    override the configurations through an app_config.h file that is
    designed to be included in a project implementation.  Defining
    the following CPP define:
    
    USE_APP_CONFIG
    
    Causes the standard configuration files to include this config
    file at the very start of each file.
    
    This update adds the CPP define to the list of defines that are
    passed to all compiled source files in order to enable the use of
    a local configuration file.
    
    An app_config.h file is also introduced by this commit to hold
    configurations specified to the SanchoDongle implementation.
    
    The initial configuration option included through this file is a
    definition which sets the product name for the USB CDC_ACM device
    that is implemented to the following:
    
    Enjellic SanchoMCU

---
+++ Sancho/Nordic/NRF52840-DONGLE/Sancho/app_config.h
@@ -0,0 +1,12 @@
+/** \file
+ * This file contains configuration overrides for the Nordic development
+ * kit for the Sancho implementation.
+ */
+
+/**************************************************************************
+ * (C)Copyright 2022, Enjellic Systems Development, LLC. All rights reserved.
+ **************************************************************************/
+
+
+/* Define the product name for the USB CDC-ACM device. */
+#define APP_USBD_STRINGS_PRODUCT APP_USBD_STRING_DESC("Enjellic SanchoMCU")
