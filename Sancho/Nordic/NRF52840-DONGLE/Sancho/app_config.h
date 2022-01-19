9b984f490750480026298104503382f814a86cb5
Dr. Greg <greg@enjellic.com>
2022-01-19 03:31:04 -0600

    Move console disablement commands to application config file.
    The defines that are used to disable the NRF CLI implementation
    that had been previously specified in the DEFINES makefile
    variable were moved to the application configuration file.

--- Sancho/Nordic/NRF52840-DONGLE/Sancho/app_config.h
+++ Sancho/Nordic/NRF52840-DONGLE/Sancho/app_config.h
@@ -8,5 +8,12 @@
  **************************************************************************/
 
 
+/* Turn off the console command-line interface. */
+#define NRF_CLI_UART_ENABLED		0
+#define NRF_CLI_LOG_BACKEND		0
+#define NRF_LOG_BACKEND_UART_ENABLED	0
+#define NRF_LOG_ENABLED			0
+
+
 /* Define the product name for the USB CDC-ACM device. */
 #define APP_USBD_STRINGS_PRODUCT APP_USBD_STRING_DESC("Enjellic SanchoMCU")
