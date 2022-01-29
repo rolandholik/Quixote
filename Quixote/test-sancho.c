8c488a10863b7c250d54e5fd37c4b83d05574552
Dr. Greg <greg@enjellic.com>
2022-01-28 20:37:17 -0600

    Send reset command on program termination.
    The test-sancho utility was modified to send a reset command when
    the end of the input stream is detected.  This will insure that
    the STmicro implementation is returned to a status of waiting for
    a connection.

--- Quixote/test-sancho.c
+++ Quixote/test-sancho.c
@@ -52,6 +52,35 @@ static double wall_time(void)
 /**
  * Private function.
  *
+ * This function is responsible for issueing a reset request to the
+ * Sancho micro-controller implementation.
+ *
+ * \param duct		A pointer to the object being used to communicate
+ *			with the micro-controller.
+ *
+ * \param bufr		The object which will be used to hold the command
+ *			to be sent.
+ *
+ * \return	No return value is defined.
+ */
+
+static void send_reset(CO(TTYduct, duct), CO(Buffer, bufr))
+
+{
+	static unsigned char cmd[] = "reset";
+
+
+	bufr->reset(bufr);
+	if ( bufr->add(bufr, cmd, sizeof(cmd)) )
+		duct->send_Buffer(duct, bufr);
+
+	return;
+}
+
+
+/**
+ * Private function.
+ *
  * This function is responsible for receiving a set of contour points
  * from the sancho.
  *
@@ -424,6 +453,9 @@ extern int main(int argc, char *argv[])
 
 
  done:
+	if ( duct != NULL )
+		send_reset(duct, bufr);
+
 	WHACK(duct);
 	WHACK(bufr);
 
