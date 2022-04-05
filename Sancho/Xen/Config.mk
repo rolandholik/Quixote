bb6e4c7a22a54ad8ff1a73a072f3d09b78ee5874
Dr. Greg <greg@enjellic.com>
2022-04-05 17:53:53 -0500

    Initial implement of mini-os based HurdLib.
    This commit adds the HurdLib directory to build a version of the
    HurdLib library against the mini-os and newlib implementation.
    
    Following the lead of other architectures the HurdLib build
    consists of a Makefile that represents the standard userspace
    implementation of library.  The Makefile implements the necessary
    defines and directives to build the library against the embedded
    implementation.
    
    This commit also introduces the Config.mk include file that will
    contain directives specific to the Xen stubdom target
    implementation.  Use of this to avoid multiple definitions of
    common build infrastructure will be expanded in subsequent
    updates.

---
+++ Sancho/Xen/Config.mk
@@ -0,0 +1,14 @@
+# **************************************************************************
+# * Copyright (c) Enjellic Systems Development, LLC. All rights reserved.
+# *
+# * Please refer to the file named Documentation/COPYRIGHT in the top of
+# * the source tree for copyright and licensing information.
+# **************************************************************************
+
+
+# Variable declarations.
+CC=gcc
+
+RANLIB = ranlib
+
+PLATFORM = -mno-red-zone -O1
