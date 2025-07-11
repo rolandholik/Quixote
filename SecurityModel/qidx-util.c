4887b50a374d9590dc1695086d64831f235e2e25
Dr. Greg <greg@enjellic.com>
2025-07-11 02:05:05 -0500

    Implement dump mode for index utility.
    This commit adds the ability to dump out the documents in an
    index.  This function is selected by the -D command-line argument
    to the qidx-util tool.
    
    This update to the utility implements 'scroll' mode in order to
    dump an index regardless of the number of documents in the index.
    By default, searching for documents in an index is limited to
    10,000 documents and it is not uncommon for Quixote event indexes
    to have millions of security event descriptions in them.
    
    The dumping of an index is encapsulated in the dump_index()
    function.  This function uses the following helper functions to
    implement the dumping:
    
    _dump_array()
    _scroll()
    
    The dump_array() function creates a JSON document that specifies
    the number of documents in a dump 'window' and the search
    specification.  Since this is a 'dump' the search specification
    is to match all of the documents in an index.
    
    In response to processing of this document the indexing engine
    returns a JSON document that defines the total number of
    documents that match the search specification and the first
    'window' of documents encoded as a JSON array of JSON objects.
    
    The _dump_array() help function extracts the array of JSON
    objects and prints the ASCII representation of each document.
    
    The JSON response document contains a string value that is named
    by the key name of '_scroll_id'.  This Base64 encoded string is a
    cursor that is returned to the indexing engine to select the next
    'window' of documents.
    
    The total number of documents that match the search is decomposed
    into the number of scroll cycles that are needed to dump all of
    the documents that match the search specification.
    
    The _scroll() helper function is called to encode the _scroll_id
    in a JSON document that is submitted to the index engine to
    select the next window of documents.  The _dump_array() function
    is called by the _scroll() helper function to dump the array of
    documents that is returned in response to the scroll request.

--- SecurityModel/qidx-util.c
+++ SecurityModel/qidx-util.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 
 #include <HurdLib.h>
@@ -18,6 +19,230 @@
 
 #include "NAAAIM.h"
 #include "ESclient.h"
+#include "JSONparser.h"
+
+
+/**
+ * Internal helper function.
+ *
+ * This function is a helper function for the dump_index function.  It
+ * outputs a set of security event descriptions held in a parser
+ * array.
+ *
+ * \param parser	The parser object that contains an array of
+ *			descriptions to be output.
+ *
+ * \param str		The object that will be used to extract and
+ *			print each member of the array.
+ *
+ * \return		A boolean value is returned to indicate whether
+ *			or not the output of the array succeeded.  A
+ *			true value indicates the contents was dumped
+ *			while a false value indicates an error occurred.
+ */
+
+static _Bool _dump_array(CO(JSONparser, parser), CO(String, str))
+
+{
+	_Bool retn = false;
+
+	int lp,
+	    cnt;
+
+
+	if ( !parser->get_array_size(parser, &cnt) )
+		ERR(goto done);
+
+	for (lp= 0; lp < cnt; ++lp) {
+		if ( !parser->select_array_object(parser, lp) )
+			ERR(goto done);
+		if ( !parser->select_object(parser, "_source") )
+			ERR(goto done);
+
+		str->reset(str);
+		if ( !parser->get_object_String(parser, str) )
+			ERR(goto done);
+		str->print(str);
+	}
+	retn = true;
+
+
+ done:
+	return retn;
+}
+
+
+/**
+ * Internal helper function.
+ *
+ * This function is a helper function that executes a scroll cycle.
+ *
+ * \param client	The object describing that endpoint that is
+ *			being scrolled.
+
+ * \param parser	The parser object that will be used to parse
+ *			the output of the scroll transaction.
+ *
+ * \param bufr		The object that will contain the output of
+ *			the scroll transaction.
+ *
+ * \param str		The object that will be used to extract the
+ *			scroll id.
+ *
+ * \param scroll	The object that will be used to create the
+ *			scroll command.
+ *
+ * \return		A boolean value is returned to indicate whether
+ *			or not the the scroll succeeded.  A true value
+ *			indicates that the scroll cycle was successfully
+ *			completed while a false value indicates an
+ *			error was encountered.
+ */
+
+static _Bool _scroll(CO(ESclient, client), CO(JSONparser, parser), \
+		     CO(Buffer, bufr), CO(String, str), CO(String, scroll))
+
+{
+	_Bool retn = false;
+
+
+	if ( !scroll->add_sprintf(scroll, "{\"scroll\": \"30s\", ") )
+		ERR(goto done);
+	if ( !scroll->add_sprintf(scroll, "\"scroll_id\": \"%s\"}", \
+				  str->get(str)) )
+		ERR(goto done);
+
+	bufr->reset(bufr);
+	if ( !client->next(client, scroll) )
+		ERR(goto done);
+
+	parser->reset(parser);
+	if ( !parser->parse_Buffer(parser, bufr) )
+		ERR(goto done);
+	if ( !parser->select_object(parser, "hits") )
+		ERR(goto done);
+	if ( !parser->select_array(parser, "hits") )
+		ERR(goto done);
+
+	if ( !_dump_array(parser, str) )
+		ERR(goto done);
+
+	str->reset(str);
+	parser->clear(parser);
+	if ( !parser->select_string(parser, "_scroll_id") )
+		ERR(goto done);
+	if ( !parser->get_String(parser, str) )
+		ERR(goto done);
+	retn = true;
+
+
+ done:
+	return retn;
+}
+
+
+/**
+ * Internal private function.
+ *
+ * This function outputs the documents contained in an index using the
+ * scroll API.
+ *
+ * \param client	The index client that is to be dumped.
+ *
+ * \param bufr		The object that will contain the output from
+ *			exchanges with the document index engine.
+ *
+ * \return		A boolean value is returned to indicate whether
+ *			or not the .  A false value
+ *			indicates a parsing error while a true value
+ *			indicates the structure pointed to by the fp
+ *			argument was properly populated.
+ */
+
+static _Bool dump_index(CO(ESclient, client), CO(Buffer, bufr))
+
+{
+	_Bool retn = false;
+
+	unsigned int lp;
+
+	int total, cnt, cycles, residual;
+
+	String str    = NULL,
+	       scroll = NULL;
+
+	JSONparser parser = NULL;
+
+
+	INIT(HurdLib, String, str, ERR(goto done));
+	if ( !str->add(str, "{\"size\": 100, ") )
+		ERR(goto done);
+	if ( !str->add(str, "\"query\": {\"match_all\": {}}}") )
+		ERR(goto done);
+	if ( !client->scroll(client, str) )
+		ERR(goto done);
+
+	INIT(NAAAIM, JSONparser, parser, ERR(goto done));
+	if ( !parser->parse_Buffer(parser, bufr) )
+		ERR(goto done);
+
+	parser->clear(parser);
+	if ( !parser->select_object(parser, "hits") )
+		ERR(goto done);
+
+	if ( !parser->select_array(parser, "hits") )
+		ERR(goto done);
+
+	if ( !parser->select_object(parser, "total") )
+		ERR(goto done);
+	if ( !parser->select_integer(parser, "value") )
+		ERR(goto done);
+	if ( !parser->get_integer(parser, &total) )
+		ERR(goto done);
+
+	if ( !_dump_array(parser, str) )
+		ERR(goto done);
+
+	if ( !parser->get_array_size(parser, &cnt) )
+		ERR(goto done);
+	if ( cnt == total ) {
+		retn = true;
+		goto done;
+	}
+
+	/* Scroll through the remaining entries. */
+	total	-= cnt;
+	cycles	 = total / 100;
+	residual = total % 100;
+
+	str->reset(str);
+	parser->clear(parser);
+	if ( !parser->select_string(parser, "_scroll_id") )
+		ERR(goto done);
+	if ( !parser->get_String(parser, str) )
+		ERR(goto done);
+
+	INIT(HurdLib, String, scroll, ERR(goto done));
+
+	for (lp= 0; lp < cycles; ++lp) {
+		if ( !_scroll(client, parser, bufr, str, scroll) )
+			ERR(goto done);
+	}
+
+	if ( residual != 0 ) {
+		if ( !_scroll(client, parser, bufr, str, scroll) )
+			ERR(goto done);
+	}
+	retn = true;
+
+
+ done:
+	WHACK(str);
+	WHACK(scroll);
+	WHACK(parser);
+
+	return retn;
+}
 
 
 /*
@@ -37,7 +262,8 @@ int main(int argc, char *argv[])
 
 	enum {
 		no_mode,
-		list_mode
+		list_mode,
+		dump_mode
 	} mode = no_mode;
 
 	Buffer bufr = NULL;
@@ -46,8 +272,11 @@ int main(int argc, char *argv[])
 
 
 	/* Parse and validate arguments. */
-	while ( (opt = getopt(argc, argv, "Lh:i:p:u:")) != EOF )
+	while ( (opt = getopt(argc, argv, "DLh:i:p:u:")) != EOF )
 		switch ( opt ) {
+			case 'D':
+				mode = dump_mode;
+				break;
 			case 'L':
 				mode = list_mode;
 				break;
@@ -103,6 +332,13 @@ int main(int argc, char *argv[])
 			if ( !bufr->add(bufr, (void *) "\0", 1) )
 				ERR(goto done);
 			fprintf(stdout, "%s", bufr->get(bufr));
+			break;
+
+		case dump_mode:
+			if ( !dump_index(client, bufr) ) {
+				fputs("Index dump failed.\n", stderr);
+				goto done;
+			}
 			break;
 
 		default:
